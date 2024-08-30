ac508864f085f56f72ccef72fc94c1caa9eee4d6
grapefruit13 <liam.heo11@gmail.com>
2024-08-30 22:57:30 +0900

    Refactor: Button size variant height 대신 py로 변경

--- src/components/Button/Button.variant.tsx
+++ src/components/Button/Button.variant.tsx
@@ -1,21 +1,21 @@
 import { cva } from 'class-variance-authority';
 
 export const buttonVariants = cva(
-  'inline-flex items-center justify-center rounded-lg focus-visible:outline-none  disabled:opacity-50 disabled:cursor-not-allowed',
+  'inline-flex items-center justify-center font-bold rounded-lg focus-visible:outline-none  disabled:opacity-50 disabled:cursor-not-allowed',
   {
     variants: {
       variant: {
         primary:
-          'bg-buttonPrimary text-primary font-bold hover:bg-buttonPrimaryHover active:bg-buttonPrimaryActive',
+          'bg-buttonPrimary text-primary hover:bg-buttonPrimaryHover active:bg-buttonPrimaryActive',
         secondary:
-          'bg-buttonSecondary text-label font-bold hover:bg-buttonSecondaryHover active:bg-buttonSecondaryActive',
+          'bg-buttonSecondary text-label hover:bg-buttonSecondaryHover active:bg-buttonSecondaryActive',
         outline:
-          'text-buttonOutline font-bold border-[1px] border-solid border-buttonOutline hover:border-buttonOutlineHover hover:text-buttonOutlineHover active:border-buttonOutlineActive active:text-buttonOutlineActive',
+          'text-buttonOutline border-[1px] border-solid border-buttonOutline hover:border-buttonOutlineHover hover:text-buttonOutlineHover active:border-buttonOutlineActive active:text-buttonOutlineActive',
       },
       size: {
-        small: 'h-10 px-4 text-xs',
-        basic: 'h-12 px-5 text-sm',
-        large: 'h-[60px] px-6 text-base',
+        small: 'py-2 px-4 text-xs',
+        basic: 'py-3 px-5 text-sm',
+        large: 'py-4 px-6 text-base',
       },
       width: {
         fit: 'w-fit',
