3e4278a1e77aa9de84f7b1ed923c267b84a7c8dc
grapefruit13 <liam.heo11@gmail.com>
2024-08-30 19:22:12 +0900

    Chore: button 컬러 변수값 추가

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,11 @@
 import type { Config } from 'tailwindcss';
 
+const colors = {
+  buttonOutline: '#4880FF',
+  buttonOutlineHover: '#2D6CFC',
+  buttonOutlineActive: '#0F58FE',
+};
+
 const config: Config = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
@@ -15,16 +21,28 @@ const config: Config = {
         button: '#FAFBFD',
         select: '#F9F9FB',
         apply: '#E2EAF8',
+        buttonPrimary: '#4379EE',
+        buttonPrimaryHover: '#2B64E0',
+        buttonPrimaryActive: '#195CEB',
+        buttonSecondary: '#E2EAF8',
+        buttonSecondaryHover: '#D7DFEC',
+        buttonSecondaryActive: '#CBD1DD',
       },
       textColor: {
         primary: '#FDFDFD',
         white: '#272727',
         label: '#060606',
+        buttonOutline: colors.buttonOutline,
+        buttonOutlineHover: colors.buttonOutlineHover,
+        buttonOutlineActive: colors.buttonOutlineActive,
       },
       borderColor: {
         calendar: '#3F3F3F',
         input: '#D5D5D5',
         card: '#B9B9B9',
+        buttonOutline: colors.buttonOutline,
+        buttonOutlineHover: colors.buttonOutlineHover,
+        buttonOutlineActive: colors.buttonOutlineActive,
       },
     },
   },
