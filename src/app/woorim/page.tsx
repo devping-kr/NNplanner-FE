88379c651b36eda8215b5e951a0a27a2136a8dc1
grapefruit13 <liam.heo11@gmail.com>
2024-08-31 01:22:48 +0900

    Feat: Icon 테스트 페이지에서 import

--- src/app/woorim/page.tsx
+++ src/app/woorim/page.tsx
@@ -1,10 +1,19 @@
 'use client';
 
 import Button from '@/components/Button/Button';
+import Icon from '@/components/Icon';
+import { Search } from '@/components/icons';
+import Edit from '@/components/icons/Edit';
 
 const page = () => {
   return (
     <div className='flex flex-col gap-4 p-4 bg-slate-600 w-full h-full'>
+      <div className='flex gap-4 items-end'>
+        <Icon width={14} height={14} stroke='red' icon={<Search />} />
+        <Icon padding='basic' fill='blue' icon={<Edit />} />
+        <Icon padding='large' width={24} height={24} icon={<Search />} />
+      </div>
+
       <div className='flex gap-4 items-end'>
         <Button variant='primary' size='small' width='fit'>
           SMALL PRIMARY
