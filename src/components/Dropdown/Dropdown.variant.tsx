1d435f846712de71bd2783a1aa4824f3189fb7dc
grapefruit13 <liam.heo11@gmail.com>
2024-09-01 13:01:32 +0900

    Feat: Dropdown 컴포넌트 구현

---
+++ src/components/Dropdown/Dropdown.variant.tsx
@@ -0,0 +1,21 @@
+import { cva } from 'class-variance-authority';
+
+export const dropdownVariants = cva(
+  'absolute z-10 w-full max-h-56 overflow-y-scroll custom-scrollbar bg-white border-[1px] border-red rounded-md shadow-md',
+  {
+    variants: {
+      isOpen: {
+        true: 'block',
+        false: 'hidden',
+      },
+      size: {
+        small: 'rounded-md mt-1',
+        basic: 'rounded-lg mt-[6px]',
+        large: 'rounded-lg mt-2',
+      },
+    },
+    defaultVariants: {
+      isOpen: false,
+    },
+  },
+);
