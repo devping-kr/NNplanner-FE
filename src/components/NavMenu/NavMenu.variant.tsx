757df8b2ca321eca0075ac6e8ef5657d674e90ca
plla2 <jhbljs92@naver.com>
2024-09-01 11:21:56 +0900

    Fix: hover, active 의사클래스 스타일 적용

--- src/components/NavMenu/NavMenu.variant.tsx
+++ src/components/NavMenu/NavMenu.variant.tsx
@@ -1,12 +1,12 @@
 import { cva } from 'class-variance-authority';
 
 export const navMenuVariants = cva(
-  'flex items-center gap-3 w-full px-4 py-3 text-sm bg-transparent h-11 rounded-full cursor-pointer',
+  'flex items-center gap-3 w-full px-4 py-3 text-sm bg-transparent h-11 rounded-full cursor-pointer hover:bg-navAction active:bg-navActionActive',
   {
     variants: {
       isActive: {
-        true: 'bg-NavActionBg text-navActionText',
-        false: 'text-navNoActionText',
+        true: 'bg-navAction text-navAction ',
+        false: 'text-navNoAction',
       },
     },
   },
