2c2b0816d4de05f17990f7f910a8b0069dae5691
plla2 <jhbljs92@naver.com>
2024-09-09 14:50:23 +0900

    Feat: schema, 에러메세지 작성

--- src/schema/authSchema.ts
+++ src/schema/authSchema.ts
@@ -1,7 +1,24 @@
 import { z } from 'zod';
 import { AUTH_ERROR } from '@/constants/_schema';
 
-export const loginSchema = z.object({
-  email: z.string().email({ message: AUTH_ERROR.email }),
-  password: z.string().min(8, { message: AUTH_ERROR.password }),
+const baseAuthSchema = z.object({
+  email: z.string().email({ message: AUTH_ERROR.base.email }),
+  password: z.string().min(8, { message: AUTH_ERROR.base.password }),
 });
+
+export const loginSchema = baseAuthSchema;
+
+export const signUpSchema = baseAuthSchema
+  .extend({
+    name: z.string().min(2, { message: AUTH_ERROR.signup.name }),
+    passwordConfirm: z.string().min(8, { message: AUTH_ERROR.base.password }),
+  })
+  .superRefine(({ passwordConfirm, password }, ctx) => {
+    if (password !== passwordConfirm) {
+      ctx.addIssue({
+        code: z.ZodIssueCode.custom,
+        message: '비밀번호가 일치하지 않습니다.',
+        path: ['passwordConfirm'],
+      });
+    }
+  });
