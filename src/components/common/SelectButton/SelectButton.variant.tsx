9b07c61583598a07a60ef9484830fce7fb76e0d3
grapefruit13 <liam.heo11@gmail.com>
2024-09-11 23:39:20 +0900

    fix: SelectButton min width 추가

--- src/components/common/SelectButton/SelectButton.variant.tsx
+++ src/components/common/SelectButton/SelectButton.variant.tsx
@@ -1,13 +1,13 @@
 import { cva } from 'class-variance-authority';
 
 export const selectButtonVariants = cva(
-  'w-full border-[1px] border-gray-300 rounded-md bg-white-200 text-left focus:border-green-700 focus:outline-none',
+  'w-fit border-[1px] border-gray-300 rounded-md bg-white-200 text-left focus:border-green-700 focus:outline-none',
   {
     variants: {
       size: {
-        small: 'px-3 py-2 text-sm rounded-md pr-8',
-        basic: 'px-5 py-3 text-basic rounded-lg pr-10',
-        large: 'px-7 py-5 text-lg rounded-lg pr-12',
+        small: 'min-w-20 px-3 py-2 text-sm rounded-md pr-8',
+        basic: 'min-w-24 px-5 py-3 text-basic rounded-lg pr-10',
+        large: 'min-w-28 px-7 py-5 text-lg rounded-lg pr-12',
       },
     },
   },
