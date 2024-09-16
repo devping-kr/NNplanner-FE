387c1be537f25c3dcfce140d66768066b65a8886
grapefruit13 <liam.heo11@gmail.com>
2024-09-17 01:09:05 +0900

    feat: 자동 식단 생성 식단 제목과 카테고리 스키마 및 에러메시지 정의

---
+++ src/schema/mealSchema.ts
@@ -0,0 +1,9 @@
+import { z } from 'zod';
+import { MEAL_HEADER_ERROR } from '@/constants/_schema';
+
+export const mealHeaderSchema = z.object({
+  name: z
+    .string()
+    .min(2, { message: MEAL_HEADER_ERROR.name.min })
+    .max(20, { message: MEAL_HEADER_ERROR.name.max }),
+});
