ff3cc159991073dbf6ff44d16fd0cb7cb898323c
grapefruit13 <liam.heo11@gmail.com>
2024-09-17 01:27:34 +0900

    fix: 하루당 최대 메뉴 7개로 변경에 따른 CalendarDay 스타일 수정

--- src/components/common/Calendar/Calendar.variant.tsx
+++ src/components/common/Calendar/Calendar.variant.tsx
@@ -1,11 +1,11 @@
 import { cva } from 'class-variance-authority';
 
 export const calendarDayVariants = cva(
-  'w-52 h-48 py-3 px-4 flex flex-col bg-white-100 text-left border-[1px] border-gray-200 hover:border-gray-400 active:border-gray-500',
+  'w-40 h-44 p-2 pl-4 flex flex-col text-left bg-white-100 border-[0.5px] border-gray-200 hover:border-gray-400 active:border-gray-500',
   {
     variants: {
       isInvalid: {
-        true: 'hover:border-gray-200 active:border-gray-200 text-opacity-30 text-black cursor-not-allowed ',
+        true: 'hover:border-gray-200 active:border-gray-200 text-opacity-30 text-dark-100 cursor-not-allowed',
         false: '',
       },
       isActive: {
