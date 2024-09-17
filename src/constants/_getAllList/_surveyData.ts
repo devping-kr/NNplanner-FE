be92398d1f919c3271e94a316c04ca9277a46c7f
plla2 <jhbljs92@naver.com>
2024-09-17 23:57:58 +0900

    Chore: Tab, Data 상수 추가

---
+++ src/constants/_getAllList/_surveyData.ts
@@ -0,0 +1,50 @@
+export const SURVEY_DATA = [
+  {
+    id: 1,
+    title: '설문이름1',
+    createdAt: '2024.09.14',
+    state: '진행중',
+  },
+  {
+    id: 2,
+    title: '설문이름2',
+    createdAt: '2024.09.14',
+    state: '진행중',
+  },
+  {
+    id: 3,
+    title: '설문이름3',
+    createdAt: '2024.09.14',
+    state: '마감',
+  },
+  {
+    id: 4,
+    title: '설문이름4',
+    createdAt: '2024.09.14',
+    state: '마감',
+  },
+  {
+    id: 5,
+    title: '설문이름5',
+    createdAt: '2024.09.14',
+    state: '마감',
+  },
+  {
+    id: 6,
+    title: '설문이름6',
+    createdAt: '2024.09.14',
+    state: '진행중',
+  },
+  {
+    id: 7,
+    title: '설문이름7',
+    createdAt: '2024.09.14',
+    state: '마감',
+  },
+  {
+    id: 8,
+    title: '설문이름8',
+    createdAt: '2024.09.14',
+    state: '진행중',
+  },
+];
