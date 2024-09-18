c600dd6fa5d3cf93193de9e8cb11285bea6c4fd5
grapefruit13 <liam.heo11@gmail.com>
2024-09-18 17:45:15 +0900

    feat: MealInfoContainer 컴포넌트 구현

---
+++ src/components/shared/Meal/MealInfoContainer/index.tsx
@@ -0,0 +1,19 @@
+import { NutritionDate } from '@/components/common/Typography';
+
+type MealInfoContainerProps = {
+  children: React.ReactNode;
+  date: string;
+};
+
+const MealInfoContainer = ({ children, date }: MealInfoContainerProps) => {
+  return (
+    <div className='flex h-fit w-80 flex-col gap-1 rounded-md border-[1px] border-green-400 bg-white-100 p-4'>
+      <div className='text-center'>
+        <NutritionDate>{date}</NutritionDate>
+      </div>
+      {children}
+    </div>
+  );
+};
+
+export default MealInfoContainer;
