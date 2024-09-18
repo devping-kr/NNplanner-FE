5bf355b684a8e39df6b70070e495a07a2d934c8f
grapefruit13 <liam.heo11@gmail.com>
2024-09-18 17:49:40 +0900

    refactor: AutoPlan 컴포넌트 form fieldset 적용, 사용하지 않는 함수 삭제, 객체 분리

--- src/components/feature/AutoPlan/index.tsx
+++ src/components/feature/AutoPlan/index.tsx
@@ -5,29 +5,8 @@ import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form';
 import { mealHeaderSchema } from '@/schema/mealSchema';
 import MealCalendar from '@/components/shared/Meal/MealCalender';
-import MealHeader, {
-  schoolLevelList,
-} from '@/components/shared/Meal/MealHeader';
-
-const categories = [
-  schoolLevelList,
-  [
-    { value: '학교', label: '학교' },
-    { value: '학교명', label: '학교명' },
-    { value: '병원', label: '병원' },
-  ],
-  [
-    { value: '세번쨰', label: '세번쨰' },
-    { value: '학교명', label: '학교명' },
-    { value: '병원', label: '병원' },
-    { value: '세번쨰', label: '세번쨰' },
-    { value: '학교명', label: '학교명' },
-    { value: '병원', label: '병원' },
-    { value: '세번쨰', label: '세번쨰' },
-    { value: '학교명', label: '학교명' },
-    { value: '병원', label: '병원' },
-  ],
-];
+import MealHeader from '@/components/shared/Meal/MealHeader';
+import { MOCK_CATEGORY_LIST } from '@/constants/_category';
 
 const now = new Date();
 const year = now.getFullYear();
@@ -41,10 +20,6 @@ const AutoPlan = () => {
   const isCategoryEmpty = !(
     selectedCategory.organization || selectedCategory.organizationDetail
   );
-
-  const dataClick = (date: string) => {
-    console.log(date);
-  };
 
   const {
     register,
@@ -70,6 +45,7 @@ const AutoPlan = () => {
 
   const onSubmit = (data: { name: string }) => {
     if (isCategoryEmpty) return;
+    // 선택한 카테고리 + 식단이름 제출 + autoPlan/create로 이동
     console.log(data);
   };
 
@@ -78,23 +54,24 @@ const AutoPlan = () => {
       onSubmit={handleSubmit(onSubmit)}
       className='flex w-fit flex-col gap-8'
     >
-      <MealHeader
-        categories={categories}
-        month={month}
-        register={register}
-        errors={errors}
-        selectedCategory={selectedCategory}
-        handleCategoryChange={handleCategoryChange}
-        isValid={isValid}
-      />
-      <MealCalendar
-        selectedCategory={selectedCategory}
-        isValid={isValid}
-        year={year}
-        month={month}
-        readonly={true}
-        onDateClick={dataClick}
-      />
+      <fieldset>
+        <legend className='sr-only'>자동 식단 이름 및 카테고리 등록</legend>
+        <MealHeader
+          categories={MOCK_CATEGORY_LIST}
+          register={register}
+          errors={errors}
+          selectedCategory={selectedCategory}
+          handleCategoryChange={handleCategoryChange}
+          isValid={isValid}
+        />
+        <MealCalendar
+          selectedCategory={selectedCategory}
+          isValid={isValid}
+          year={year}
+          month={month}
+          readonly={true}
+        />
+      </fieldset>
     </form>
   );
 };
