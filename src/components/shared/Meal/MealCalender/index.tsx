a076877cf7f5d2ff859dbd1f472aeef2c8084fe4
grapefruit13 <liam.heo11@gmail.com>
2024-09-18 17:47:10 +0900

    feat: 식단 페이지에서 사용되는 MealCalendar 구현

--- src/components/shared/Meal/MealCalender/index.tsx
+++ src/components/shared/Meal/MealCalender/index.tsx
@@ -1,51 +1,99 @@
 'use client';
 
+import Button from '@/components/common/Button/Button';
 import Calendar, { CalendarProps } from '@/components/common/Calendar';
 import { MealCalenderTitle } from '@/components/common/Typography';
-import MealHeaderButton from '@/components/shared/Meal/MealHeaderButton';
+import MealEdit from '@/components/shared/Meal/MealEdit';
+import NutritionInfo, {
+  NutritionData,
+} from '@/components/shared/Meal/NutritionInfo';
 
 type MealCalendarProps = {
   type?: 'default' | 'create' | 'edit';
-  selectedCategory: {
+  selectedCategory?: {
     organization: string | null;
     organizationDetail: string | null;
   };
-  isValid: boolean;
+  isValid?: boolean;
+  selectedDate?: string;
+  handleChangeMenu?: (
+    date: string,
+    menuName: string,
+    updatedItem: NutritionData,
+  ) => void;
 } & CalendarProps;
 
 const MealCalendar = ({
   type = 'default',
   selectedCategory,
   isValid,
+  selectedDate,
   year,
   month,
   data,
   readonly,
   onDateClick,
+  handleChangeMenu,
 }: MealCalendarProps) => {
   const isCategoryEmpty =
     !selectedCategory?.organization || !selectedCategory?.organizationDetail;
 
   return (
-    <div className='flex w-fit flex-col gap-4'>
-      <div className='flex w-full justify-between'>
-        <MealCalenderTitle>{month}월</MealCalenderTitle>
-        {type === 'default' && (
-          <MealHeaderButton disabled={isCategoryEmpty || !isValid}>
-            생성
-          </MealHeaderButton>
+    <div className='flex gap-8'>
+      <div className='flex w-fit flex-col gap-4'>
+        <div className='flex w-full justify-between'>
+          <MealCalenderTitle>{month}월</MealCalenderTitle>
+          {type === 'default' && (
+            <Button
+              className='h-10 w-fit'
+              size='large'
+              type='submit'
+              disabled={isCategoryEmpty || !isValid}
+            >
+              생성
+            </Button>
+          )}
+          {type === 'create' && (
+            <div className='flex w-fit gap-2'>
+              <Button className='h-10 w-fit' size='large' type='submit'>
+                저장
+              </Button>
+              <Button className='h-10 w-fit' size='large' type='button'>
+                수정
+              </Button>
+            </div>
+          )}
+          {type === 'edit' && (
+            <div className='flex w-fit gap-2'>
+              <Button className='h-10 w-fit' size='large' type='submit'>
+                수정 완료
+              </Button>
+              <Button className='h-10 w-fit' size='large' type='button'>
+                취소
+              </Button>
+            </div>
+          )}
+        </div>
+        <Calendar
+          year={year}
+          month={month}
+          data={data}
+          readonly={readonly}
+          onDateClick={onDateClick}
+        />
+      </div>
+      <div className='mt-[56px]'>
+        {type === 'create' && selectedDate && data && (
+          <NutritionInfo date={selectedDate} data={data?.[selectedDate]} />
         )}
-        {type === 'create' && (
-          <div className='flex w-fit gap-2'>{/* 추후 추가 예정 */}</div>
+        {type === 'edit' && selectedDate && data && (
+          <MealEdit
+            date={selectedDate}
+            data={data?.[selectedDate]}
+            handleChangeMenu={handleChangeMenu}
+          />
         )}
       </div>
-      <Calendar
-        year={year}
-        month={month}
-        data={data}
-        readonly={readonly}
-        onDateClick={onDateClick}
-      />
     </div>
   );
 };
