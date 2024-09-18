47a681d64cf4da8b64938e75e0b519912567753e
grapefruit13 <liam.heo11@gmail.com>
2024-09-18 17:14:10 +0900

    refactor: 테이블 컴포넌트 w-full 수정 및 head와 body className prop에 추가

--- src/components/common/Table/index.tsx
+++ src/components/common/Table/index.tsx
@@ -12,18 +12,30 @@ export type TableType = 'table' | 'list';
 type TableProps = {
   data: TableRowData[];
   type?: TableType;
+  headerClassName?: string;
+  bodyClassName?: string;
 };
 
-const Table = ({ data, type = 'table' }: TableProps) => {
+const Table = ({
+  data,
+  type = 'table',
+  headerClassName,
+  bodyClassName,
+}: TableProps) => {
   const tableHeaders = Array.from(
     new Set(data.flatMap((item) => Object.keys(item))),
   );
 
   return (
-    <div className='overflow-hidden rounded-md'>
+    <div className='w-full overflow-hidden rounded-md'>
       <table className='w-full border-collapse text-center'>
-        <TableHeader headerData={tableHeaders} />
-        <TableBody headerData={tableHeaders} bodyData={data} type={type} />
+        <TableHeader headerData={tableHeaders} className={headerClassName} />
+        <TableBody
+          headerData={tableHeaders}
+          bodyData={data}
+          type={type}
+          className={bodyClassName}
+        />
       </table>
     </div>
   );
