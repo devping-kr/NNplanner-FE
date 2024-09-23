31f5dcb484f126f363b7e7982d5512f23805b038
plla2 <jhbljs92@naver.com>
2024-09-23 12:59:51 +0900

    Fix: 빌드에러 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,6 @@ export default function Home() {
   return (
     <div className='flex flex-col gap-20'>
       <Link href={'/seunghyun'}>승현 작업</Link>
-      <Link href={'/woorim'}>우림 작업</Link>
     </div>
   );
 }
