b27a8f082892ebb17c50568a7b82335997aa7f79
plla2 <jhbljs92@naver.com>
2024-09-27 17:34:27 +0900

    Feat: auth api페칭로직 작성

--- src/api/auth/index.ts
+++ src/api/auth/index.ts
@@ -1,15 +1,45 @@
 import { post } from '@/lib/axios';
-import { SuccessResponse } from '@/type/response';
-import { UserRequest } from '@/hooks/auth/usePostSignup';
+import { LoginRequest, SignupRequest } from '@/type/auth/authRequest';
+import { LoginResponse } from '@/type/auth/authResponse';
+import { Result } from '@/type/response';
+import { saveTokens } from '@/utils/saveTokens';
 
-const signUp = async (request: UserRequest) => {
-  const response = await post<SuccessResponse<undefined>>(
-    '/api/auths/signup',
+const signUp = async (request: SignupRequest) => {
+  const response = await post<Result<null>>('/api/auths/signup', request);
+  return response.data;
+};
+
+const verifySend = async (request: { email: string }) => {
+  const response = await post<Result<null>>('/api/auths/send', request);
+  return response.data;
+};
+
+const verifyConfirm = async (request: {
+  email: string;
+  verifyCode: string;
+}) => {
+  const response = await post<Result<null>>('/api/auths/verify', request);
+  return response.data;
+};
+
+const login = async (request: LoginRequest) => {
+  const response = await post<Result<LoginResponse>>(
+    '/api/auths/login',
     request,
   );
+  saveTokens(response.data.data);
+  return response.data.data;
+};
+
+const logout = async () => {
+  const response = await post<Result<null>>('/api/auths/logout');
   return response.data;
 };
 
 export const auth = {
   signUp,
+  verifySend,
+  verifyConfirm,
+  login,
+  logout,
 };
