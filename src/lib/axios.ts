4946a9b4bc075357aaa38fba709aa39255f054fa
plla2 <jhbljs92@naver.com>
2024-09-27 17:36:45 +0900

    Feat: axios interceptors 설정

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,7 +1,22 @@
 import axios, { AxiosResponse } from 'axios';
+import Cookies from 'js-cookie';
+import { saveTokens } from '@/utils/saveTokens';
 import { env } from './env';
 
 const TIME_OUT = 1000 * 10;
+
+const redirectToLogin = () => {
+  if (typeof window !== 'undefined' && window.location.pathname !== '/login') {
+    window.location.href = '/login';
+  }
+};
+
+const getAccessToken = (): string | null => {
+  if (typeof window !== 'undefined') {
+    return localStorage.getItem('accessToken');
+  }
+  return null;
+};
 
 const instance = axios.create({
   baseURL: env.BASE_API_URL,
@@ -13,22 +28,102 @@ const instance = axios.create({
 });
 
 instance.interceptors.request.use(
-  function fullfilledRequestInterceptor(config) {
+  (config) => {
+    const accessToken = getAccessToken();
+
+    if (accessToken) {
+      config.headers.Authorization = `Bearer ${accessToken}`;
+    }
     return config;
   },
-  function rejectedRequestInterceptor(error) {
+  (error) => Promise.reject(error),
+);
+
+instance.interceptors.response.use(
+  (response) => response,
+
+  async (error) => {
+    const originalRequest = error.config;
+
+    if (
+      error.response?.status === 401 &&
+      originalRequest &&
+      !originalRequest._retry
+    ) {
+      originalRequest._retry = true;
+
+      if (typeof window !== 'undefined') {
+        const refreshToken = Cookies.get('refreshToken');
+        if (refreshToken) {
+          try {
+            const response = await instance.get('/api/auths/reissue', {
+              withCredentials: true,
+            });
+
+            if (response.status === 200) {
+              const { accessToken, refreshToken } = response.data.data;
+
+              saveTokens({ accessToken, refreshToken });
+              originalRequest.headers.Authorization = `Bearer ${accessToken}`;
+              return await instance(originalRequest);
+            }
+          } catch (refreshError) {
+            alert('로그인이 필요합니다.');
+          }
+        }
+      }
+      redirectToLogin();
+    }
     return Promise.reject(error);
   },
 );
 
-instance.interceptors.response.use(
-  function fullfilledResponseInterceptor(response) {
-    return response;
-  },
-  function rejectedResponseInterceptor(error) {
-    return Promise.reject(error);
-  },
-);
+// 디버깅 용 인터셉터
+// instance.interceptors.response.use(
+//   (response) => {
+//     console.log('Response:', response);
+//     return response;
+//   },
+//   async (error) => {
+//     console.error('Error in response interceptor:', error);
+//     const originalRequest = error.config;
+
+//     if (
+//       error.response?.status === 401 &&
+//       originalRequest &&
+//       !originalRequest._retry
+//     ) {
+//       originalRequest._retry = true;
+
+//       console.log('401 error, retrying with refreshed token...');
+//       if (typeof window !== 'undefined') {
+//         const refreshToken = Cookies.get('refreshToken');
+//         console.log('Refresh Token:', refreshToken);
+//         if (refreshToken) {
+//           try {
+//             const response = await instance.get('/api/auths/reissue', {
+//               withCredentials: true,
+//             });
+//             console.log('Reissue response:', response);
+
+//             if (response.status === 200) {
+//               const { accessToken, refreshToken } = response.data.data;
+//               saveTokens({ accessToken, refreshToken });
+
+//               originalRequest.headers.Authorization = `Bearer ${accessToken}`;
+//               return await instance(originalRequest);
+//             }
+//           } catch (refreshError) {
+//             console.error('Refresh token failed', refreshError);
+//           }
+//         }
+//       }
+//       redirectToLogin();
+//     }
+
+//     return Promise.reject(error);
+//   },
+// );
 
 export const get = <T>(...args: Parameters<typeof instance.get>) => {
   return instance.get<T, AxiosResponse<T>>(...args);
