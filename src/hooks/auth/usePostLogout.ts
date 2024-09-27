ab0d87042d62e32a26405f6799cafc5639666c72
plla2 <jhbljs92@naver.com>
2024-09-27 17:35:08 +0900

    Feat: api로직들 커스텀훅 작성

--- src/hooks/auth/usePostLogout.ts
+++ src/hooks/auth/usePostLogout.ts
@@ -1,24 +1,25 @@
 import { useRouter } from 'next/navigation';
 import { useMutation } from '@tanstack/react-query';
 import { AxiosError } from 'axios';
+import Cookies from 'js-cookie';
 import { auth } from '@/api/auth';
-import { SignupRequest } from '@/type/auth/authRequest';
 import { FailResponse, Result } from '@/type/response';
 import { useToastStore } from '@/stores/useToastStore';
 
-export const usePostSignup = () => {
+export const usePostLogout = () => {
   const showToast = useToastStore((state) => state.showToast);
   const router = useRouter();
 
   return useMutation({
-    mutationFn: (request: SignupRequest) =>
-      auth.signUp({ ...request, loginType: 'LOCAL' }),
+    mutationFn: () => auth.logout(),
     onSuccess: ({ message }: Result<null>) => {
       router.push('/login');
       showToast(message, 'success', 1000);
+      localStorage.removeItem('accessToken');
+      Cookies.remove('refreshToken');
     },
     onError: (error: AxiosError<FailResponse>) => {
-      const errorMessage = error.response?.data?.message || '회원가입 실패';
+      const errorMessage = error.response?.data?.message || '로그아웃 실패';
       showToast(errorMessage, 'warning', 1000);
     },
   });
--- src/hooks/auth/usePostVerifyConfirm.ts
+++ src/hooks/auth/usePostVerifyConfirm.ts
@@ -1,24 +1,20 @@
-import { useRouter } from 'next/navigation';
 import { useMutation } from '@tanstack/react-query';
 import { AxiosError } from 'axios';
 import { auth } from '@/api/auth';
-import { SignupRequest } from '@/type/auth/authRequest';
 import { FailResponse, Result } from '@/type/response';
 import { useToastStore } from '@/stores/useToastStore';
 
-export const usePostSignup = () => {
+export const usePostVerifyConfirm = () => {
   const showToast = useToastStore((state) => state.showToast);
-  const router = useRouter();
 
   return useMutation({
-    mutationFn: (request: SignupRequest) =>
-      auth.signUp({ ...request, loginType: 'LOCAL' }),
+    mutationFn: (request: { email: string; verifyCode: string }) =>
+      auth.verifyConfirm(request),
     onSuccess: ({ message }: Result<null>) => {
-      router.push('/login');
       showToast(message, 'success', 1000);
     },
     onError: (error: AxiosError<FailResponse>) => {
-      const errorMessage = error.response?.data?.message || '회원가입 실패';
+      const errorMessage = error.response?.data?.message || '이메일 인증 실패';
       showToast(errorMessage, 'warning', 1000);
     },
   });
