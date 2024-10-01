8c44fa0c799dbf1aa09d0676cd7cf863f6e75bba
grapefruit13 <liam.heo11@gmail.com>
2024-10-02 00:49:54 +0900

    Fix: MealCreateHeader-자동식단 생성 페이지 제목 적용, input height basic 적용 및 글자 크기 축소, selectbox small 적용

--- src/components/shared/Meal/MealCreateHeader/index.tsx
+++ src/components/shared/Meal/MealCreateHeader/index.tsx
@@ -1,5 +1,6 @@
 import { Input } from '@/components/common/Input';
 import { Selectbox } from '@/components/common/Selectbox';
+import { PageHeaderTitle } from '@/components/common/Typography';
 
 type MealCreateHeaderProps = {
   inputValue: string;
@@ -11,26 +12,30 @@ const MealCreateHeader = ({
   seletedCategory,
 }: MealCreateHeaderProps) => {
   return (
-    <div className='flex w-fit items-center gap-4'>
-      <Input
-        className='text-2xl font-semibold focus:border-green-400'
-        bgcolor='meal'
-        height='large'
-        value={inputValue}
-      />
-      <div className='flex gap-2'>
-        <Selectbox
-          size='basic'
-          selectedValue={seletedCategory[0]}
-          className='cursor-not-allowed focus:border-gray-300'
-          readonly={true}
+    <div className='flex flex-col gap-5'>
+      <PageHeaderTitle>자동 식단 생성</PageHeaderTitle>
+      <div className='flex w-fit items-center gap-4'>
+        <Input
+          className='text-lg font-semibold focus:border-green-400'
+          bgcolor='meal'
+          height='basic'
+          value={inputValue}
+          disabled
         />
-        <Selectbox
-          size='basic'
-          selectedValue={seletedCategory[1]}
-          className='cursor-not-allowed focus:border-gray-300'
-          readonly={true}
-        />
+        <div className='flex gap-2'>
+          <Selectbox
+            size='small'
+            selectedValue={seletedCategory[0]}
+            className='cursor-not-allowed focus:border-gray-300'
+            readonly={true}
+          />
+          <Selectbox
+            size='small'
+            selectedValue={seletedCategory[1]}
+            className='cursor-not-allowed focus:border-gray-300'
+            readonly={true}
+          />
+        </div>
       </div>
     </div>
   );
