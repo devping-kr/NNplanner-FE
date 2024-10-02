11f9660420871fc20cba68f924657fcf24adf9c5
grapefruit13 <liam.heo11@gmail.com>
2024-10-02 20:37:00 +0900

    Feat: 페이지별 제목 및 토스트 메시지 객체화

--- src/constants/_pageTitle.ts
+++ src/constants/_pageTitle.ts
@@ -4,4 +4,13 @@ export const PAGE_TITLE = {
     edit: '자동 식단 수정',
     create: '자동 식단 생성',
   },
+  menualPlan: {
+    default: '수동 식단 작성',
+    edit: '수동 식단 수정',
+    create: '수동 식단 생성',
+  },
+  created: {
+    default: '생성한 식단 상세',
+    edit: '생성한 식단 수정',
+  },
 };
