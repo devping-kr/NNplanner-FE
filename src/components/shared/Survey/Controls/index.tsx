844e9a973c5fbee4c20f67c65d55d2a443a477f5
plla2 <jhbljs92@naver.com>
2024-10-05 14:29:06 +0900

    Fix: pr코멘트에 따른 수정사항 적용

--- src/components/shared/Survey/Controls/index.tsx
+++ src/components/shared/Survey/Controls/index.tsx
@@ -47,7 +47,6 @@ const SurveyControls = ({
           className='font-semibold'
           bgcolor='meal'
           height='basic'
-          maxLength={29}
         />
       </div>
       <div className='relative flex w-56 items-center gap-2'>
