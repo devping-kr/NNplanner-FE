fc384d1b9c653d3626ce8bba94c9986ce68c6325
grapefruit13 <liam.heo11@gmail.com>
2024-10-09 21:10:34 +0900

    Fix: Navbar pathname에 따른 selectedTab 적용, min-w 적용, 오타 수정

--- src/components/common/Navbar/index.tsx
+++ src/components/common/Navbar/index.tsx
@@ -15,9 +15,10 @@ const Navbar = () => {
   const pathname = usePathname();
   const { logout } = useAuth();
 
-  const defualtTab = NAV_LINKS[0].name;
-  const selecedTab =
-    NAV_LINKS.find((nav) => nav.href === pathname)?.name ?? defualtTab;
+  const defaultTab = NAV_LINKS[0].name;
+  const selectedTab =
+    NAV_LINKS.find((nav) => pathname?.startsWith(`${nav.href}`))?.name ??
+    defaultTab;
 
   const handleLogout = () => {
     destroyTokens();
@@ -28,7 +29,7 @@ const Navbar = () => {
 
   return (
     !isSurveyPage && (
-      <nav className='sticky top-0 flex h-screen w-60 flex-col place-content-between bg-white-100'>
+      <nav className='sticky top-0 flex h-screen w-60 min-w-60 flex-col place-content-between bg-white-100'>
         <div className='flex flex-col gap-9 p-6'>
           <Link href='#' className='w-fit self-center'>
             <Image
@@ -41,11 +42,11 @@ const Navbar = () => {
           <div className='flex h-full flex-col'>
             <div className='flex flex-col gap-2'>
               {NAV_LINKS.map(({ name, href, icon }) => (
-                <NavMenu key={name} href={href} isActive={selecedTab === name}>
+                <NavMenu key={name} href={href} isActive={selectedTab === name}>
                   <Icon
                     name={icon}
                     className='hover:stroke-green-800'
-                    color={selecedTab === name ? 'active' : 'normal'}
+                    color={selectedTab === name ? 'active' : 'normal'}
                   />
                   <span>{name}</span>
                 </NavMenu>
