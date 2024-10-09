9f8400fa464f9a9932f5ae85f4276a343ae17e68
plla2 <jhbljs92@naver.com>
2024-10-09 16:34:23 +0900

    Fix: pre-push 빌드 에러 수정

--- src/constants/_schema.ts
+++ src/constants/_schema.ts
@@ -4,7 +4,7 @@ export const AUTH_ERROR = {
     password: '8~16자의 영문, 숫자를 포함한 비밀번호를 입력해주세요',
   },
   signup: {
-    name: '2~5자의 한글/영문만 사용 가능합니다',
+    username: '2~5자의 한글/영문만 사용 가능합니다',
     passwordConfirm: '비밀번호가 일치하지 않습니다.',
   },
 };
