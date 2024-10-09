722fb2d42fde5c486476f7289322339bacf72326
plla2 <jhbljs92@naver.com>
2024-10-09 13:45:17 +0900

    Fix: 설문 전체리스트 페칭 디버깅용 코드 추가

--- src/api/survey/index.ts
+++ src/api/survey/index.ts
@@ -1,34 +1,33 @@
-import { get } from '@/lib/axios';
+// import { get } from '@/lib/axios';
+import { env } from '@/lib/env';
 import { Result } from '@/type/response';
 import { SurveyListResponse } from '@/type/survey/surveyResponse';
 
-const getSurveyList = async () => {
-  const response = await get<Result<SurveyListResponse>>('/api/surveys');
-  return response.data;
-};
-
 // const getSurveyList = async () => {
-//   const accessToken = localStorage.getItem('accessToken');
-
-//   const response = await fetch(
-//     'https://port-0-nnplanner-be-2aat2llv7xzes5.sel5.cloudtype.app/api/surveys',
-//     {
-//       method: 'GET',
-//       headers: {
-//         'Content-Type': 'application/json',
-//         Authorization: `Bearer ${accessToken}`,
-//       },
-//       credentials: 'include',
-//     },
-//   );
-
-//   if (!response.ok) {
-//     throw new Error('Failed to fetch survey list');
-//   }
-
-//   const data = (await response.json()) as Result<SurveyListResponse>;
-//   return data;
+//   const response = await get<Result<SurveyListResponse>>('/api/surveys');
+//   return response.data;
 // };
+
+const getSurveyList = async () => {
+  const accessToken = localStorage.getItem('accessToken');
+
+  const response = await fetch(`${env.BASE_API_URL}/api/surveys`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      'x-access-token': `Bearer ${accessToken}`,
+      // Authorization: `Bearer ${accessToken}`,
+    },
+    // credentials: 'include',
+  });
+
+  if (!response.ok) {
+    throw new Error('Failed to fetch survey list');
+  }
+
+  const data = (await response.json()) as Result<SurveyListResponse>;
+  return data;
+};
 
 export const survey = {
   getSurveyList,
--- src/components/feature/ViewChart/index.tsx
+++ src/components/feature/ViewChart/index.tsx
@@ -1,6 +1,8 @@
 'use client';
 
 import { useState } from 'react';
+import { useQuery } from '@tanstack/react-query';
+import { survey } from '@/api/survey';
 import Pagination from '@/components/common/Pagination';
 import GetAllListControls from '@/components/shared/GetAllList/Controls';
 import GetAllListHeader from '@/components/shared/GetAllList/Header';
@@ -31,13 +33,30 @@ const ViewChart = () => {
     console.log('검색 버튼 클릭');
   };
 
-  // const { data, isLoading } = useQuery({
-  //   queryKey: ['surveyList'],
-  //   queryFn: () => survey.getSurveyList(),
-  // });
-  // if (data) {
-  //   console.log(data.data.surveys);
-  // }
+  const { data, isLoading } = useQuery({
+    queryKey: ['surveyList'],
+    queryFn: () => survey.getSurveyList(),
+  });
+  if (data) {
+    console.log(data.data.surveys);
+  }
+
+  // react-query문제 디버깅용
+  // useEffect(() => {
+  //   const fetchSurveys = async () => {
+  //     try {
+  //       setIsLoading(true); // 로딩 상태 활성화
+  //       const data = await survey.getSurveyList(); // 설문 목록 불러오기
+  //       setSurveyData(data.data.surveys); // 불러온 데이터 상태에 저장
+  //     } catch (err) {
+  //       setError('Failed to fetch survey list'); // 에러 발생 시 설정
+  //     } finally {
+  //       setIsLoading(false); // 로딩 상태 비활성화
+  //     }
+  //   };
+
+  //   fetchSurveys();
+  // }, []); // 컴포넌트가 마운트될 때만 실행
 
   // 추후 react-query queryKey에 filter된 데이터 캐싱예정
   const filterSurveys = (filterTab: string) => {
@@ -47,7 +66,7 @@ const ViewChart = () => {
     return SURVEY_DATA.filter((survey) => survey.state === filterTab);
   };
 
-  // if (isLoading) return <div>Loading...</div>;
+  if (isLoading) return <div>Loading...</div>;
 
   return (
     <div className='flex flex-col gap-4'>
