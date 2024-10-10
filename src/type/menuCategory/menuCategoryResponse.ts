4c8fca26488da03015322475eb33aca5d7a5b72c
grapefruit13 <liam.heo11@gmail.com>
2024-10-10 23:17:58 +0900

    Fix: 메뉴카테고리 API response body 필요없는 타입 삭제

--- src/type/menuCategory/menuCategoryResponse.ts
+++ src/type/menuCategory/menuCategoryResponse.ts
@@ -1,25 +1,12 @@
 import { MenuResponseDTO } from '@/type/menu/menuResponse';
 
 /**
- * @description 소분류 목록 조회 response body
+ * @description 카테고리로 식단 전체 목록 조회 response body
  */
-export interface MenuMinorCategoryResponse {
-  message: string;
-  data: string[];
-}
-
-export interface MenuListByCategoriesResponseData {
+export interface MenuListByCategoriesResponse {
   currentPage: number;
   totalPages: number;
   totalElements: number;
   pageSize: number;
   menuResponseDTOList: MenuResponseDTO[];
-}
-
-/**
- * @description 카테고리로 식단 전체 목록 조회 response body
- */
-export interface MenuListByCategoriesResponse {
-  message: string;
-  data: MenuListByCategoriesResponseData;
 }
