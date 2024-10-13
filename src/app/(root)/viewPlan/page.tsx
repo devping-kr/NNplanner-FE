ad6b586648ba4d96dc05b4074fa198d46231e206
plla2 <jhbljs92@naver.com>
2024-10-13 22:20:44 +0900

    Fix: 빌드에러 수정(suspense 처리)

--- src/app/(root)/viewPlan/page.tsx
+++ src/app/(root)/viewPlan/page.tsx
@@ -1,7 +1,12 @@
+import { Suspense } from 'react';
 import ViewPlan from '@/components/feature/ViewPlan';
 
 const page = () => {
-  return <ViewPlan />;
+  return (
+    <Suspense fallback={<div>loading...</div>}>
+      <ViewPlan />
+    </Suspense>
+  );
 };
 
 export default page;
