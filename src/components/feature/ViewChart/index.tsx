a1017239a93b6961659ccbed00e1fd88733a43fa
plla2 <jhbljs92@naver.com>
2024-10-13 13:20:17 +0900

    Feat: 설문 목록조회 페이지네이션, 필터, 정렬까지 완료 (검색, 날짜선택 진행 해야함)

--- src/components/feature/ViewChart/index.tsx
+++ src/components/feature/ViewChart/index.tsx
@@ -1,26 +1,35 @@
 'use client';
 
+import { useSearchParams } from 'next/navigation';
 import { useState } from 'react';
-import { useQuery } from '@tanstack/react-query';
-import { survey } from '@/api/survey';
+import dayjs from 'dayjs';
+import { surveyType } from '@/type/survey/surveyResponse';
 import { getCurrentYearMonthNow } from '@/utils/calendar';
 import Pagination from '@/components/common/Pagination';
 import GetAllListControls from '@/components/shared/GetAllList/Controls';
 import GetAllListHeader from '@/components/shared/GetAllList/Header';
 import GetAllListTable from '@/components/shared/GetAllList/ListTable';
 import { SURVEY_FILTER_OPTIONS, TAB_OPTIONS } from '@/constants/_controlTab';
-import { SURVEY_DATA } from '@/constants/_getAllList/_surveyData';
+import { useGetSurveyList } from '@/hooks/survey/useGetSurveyList';
 
 const ViewChart = () => {
+  const searchParam = useSearchParams();
+  const sort = searchParam.get('sort') as string;
+  const currentTab = sort ?? ('최신순' as string);
+
   const { month, year } = getCurrentYearMonthNow();
+  const [searchValue, setSearchValue] = useState('');
   const [selectedYear, setSelectedYear] = useState<string>(year.toString());
   const [selectedMonth, setSelectedMonth] = useState<string>(month.toString());
-  const [searchValue, setSearchValue] = useState('');
   const [selectedFilter, setSelectedFilter] = useState<string>(
     SURVEY_FILTER_OPTIONS[0],
   );
   const [selectedTab, setSelectedTab] = useState<string>(TAB_OPTIONS[0]);
   const [page, setPage] = useState(1);
+  const { data: surveyList } = useGetSurveyList({
+    page,
+    sort: currentTab === '최신순' ? 'createdAt,desc' : 'createdAt,asc',
+  });
 
   const handlechangeSearchValue = (e: React.ChangeEvent<HTMLInputElement>) => {
     setSearchValue(e.target.value);
@@ -31,66 +40,46 @@ const ViewChart = () => {
     console.log('검색 버튼 클릭');
   };
 
-  const { data, isLoading } = useQuery({
-    queryKey: ['surveyList'],
-    queryFn: () => survey.getSurveyList(),
-  });
-  if (data) {
-    console.log(data.data.surveys);
-  }
-
-  // react-query문제 디버깅용
-  // useEffect(() => {
-  //   const fetchSurveys = async () => {
-  //     try {
-  //       setIsLoading(true); // 로딩 상태 활성화
-  //       const data = await survey.getSurveyList(); // 설문 목록 불러오기
-  //       setSurveyData(data.data.surveys); // 불러온 데이터 상태에 저장
-  //     } catch (err) {
-  //       setError('Failed to fetch survey list'); // 에러 발생 시 설정
-  //     } finally {
-  //       setIsLoading(false); // 로딩 상태 비활성화
-  //     }
-  //   };
-
-  //   fetchSurveys();
-  // }, []); // 컴포넌트가 마운트될 때만 실행
-
-  // 추후 react-query queryKey에 filter된 데이터 캐싱예정
-  const filterSurveys = (filterTab: string) => {
-    if (filterTab === '전체') {
-      return SURVEY_DATA;
-    }
-    return SURVEY_DATA.filter((survey) => survey.state === filterTab);
+  const formatSurveyList = (surveys: surveyType[]) => {
+    return surveys.map((survey) => ({
+      ...survey,
+      createdAt: dayjs(survey.createdAt).format('YYYY-MM-DD'),
+      deadlineAt: dayjs(survey.deadlineAt).format('YYYY-MM-DD'),
+      state: survey.state === 'IN_PROGRESS' ? '진행중' : '마감',
+    }));
   };
-
-  if (isLoading) return <div>Loading...</div>;
 
   return (
     <div className='flex flex-col gap-4'>
-      <GetAllListHeader title='설문 결과 리스트' />
-      <GetAllListControls
-        type='viewChart'
-        selectedMonth={selectedMonth}
-        selectedYear={selectedYear}
-        onMonthChange={setSelectedMonth}
-        onYearChange={setSelectedYear}
-        searchValue={searchValue}
-        handlechangeSearchValue={handlechangeSearchValue}
-        submitSearchValue={submitSearchValue}
-        selectedFilter={selectedFilter}
-        setSelectedFilter={setSelectedFilter}
-        selectedTab={selectedTab}
-        setSelectedTab={setSelectedTab}
-        inputPlaceholder='설문 이름을 입력해주세요.'
-      />
-      <GetAllListTable data={filterSurveys(selectedFilter)} />
-      <Pagination
-        limit={8}
-        page={page}
-        setPage={setPage}
-        totalPosts={SURVEY_DATA.length}
-      />
+      {surveyList && (
+        <>
+          <GetAllListHeader title='설문 결과 리스트' />
+          <GetAllListControls
+            type='viewChart'
+            selectedMonth={selectedMonth}
+            selectedYear={selectedYear}
+            onMonthChange={setSelectedMonth}
+            onYearChange={setSelectedYear}
+            searchValue={searchValue}
+            handlechangeSearchValue={handlechangeSearchValue}
+            submitSearchValue={submitSearchValue}
+            selectedFilter={selectedFilter}
+            setSelectedFilter={setSelectedFilter}
+            selectedTab={selectedTab}
+            setSelectedTab={setSelectedTab}
+            inputPlaceholder='설문 이름을 입력해주세요.'
+          />
+          <GetAllListTable data={formatSurveyList(surveyList.surveys)} />
+          <Pagination
+            limit={8}
+            page={page}
+            setPage={setPage}
+            totalPosts={
+              surveyList.surveys.length === 0 ? 0 : surveyList.totalItems
+            }
+          />
+        </>
+      )}
     </div>
   );
 };
