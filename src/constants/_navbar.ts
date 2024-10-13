8e28bf753da18f5ff09d8eb3a473c0561c85736d
grapefruit13 <liam.heo11@gmail.com>
2024-10-14 01:01:22 +0900

    Feat: link에 전달할 ROUTES 객체 추가

--- src/constants/_navbar.ts
+++ src/constants/_navbar.ts
@@ -1,27 +1,57 @@
+export const BASE_ROUTES = {
+  ROOT: '/',
+  AUTO: '/autoPlan',
+  MENUAL: '/menualPlan',
+  VIEW_PLAN: '/viewPlan',
+  VIEW_CHART: '/viewChart',
+  CREATE: '/create',
+  EDIT: '/edit',
+};
+
+/**
+ * @description 네비게이션 페이지 이름, 경로, 아이콘 이름
+ */
 export const NAV_LINKS = [
   {
     name: '홈 대시보드',
-    href: '/',
+    href: BASE_ROUTES.ROOT,
     icon: 'dashboard',
   },
   {
     name: '자동 식단 작성',
-    href: '/autoPlan',
+    href: BASE_ROUTES.AUTO,
     icon: 'auto',
   },
   {
     name: '수동 식단 작성',
-    href: '/menualPlan',
+    href: BASE_ROUTES.MENUAL,
     icon: 'calendar',
   },
   {
     name: '식단 조회',
-    href: '/viewPlan',
+    href: BASE_ROUTES.VIEW_PLAN,
     icon: 'search',
   },
   {
     name: '설문 결과 조회',
-    href: '/viewChart',
+    href: BASE_ROUTES.VIEW_CHART,
     icon: 'chart',
   },
 ];
+
+const createRoutes = (baseRoutes: typeof BASE_ROUTES) => ({
+  CREATE: {
+    AUTO: `${baseRoutes.AUTO}${baseRoutes.CREATE}`,
+    MENUAL: `${baseRoutes.MENUAL}${baseRoutes.CREATE}`,
+  },
+  EDIT: {
+    AUTO: `${baseRoutes.AUTO}${baseRoutes.EDIT}`,
+    MENUAL: `${baseRoutes.MENUAL}${baseRoutes.EDIT}`,
+  },
+  VIEW: {
+    PLAN: baseRoutes.VIEW_PLAN,
+    CHART: baseRoutes.VIEW_CHART,
+  },
+});
+
+export const ROUTES = createRoutes(BASE_ROUTES);
