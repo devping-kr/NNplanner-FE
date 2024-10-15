c224f7e0abe09f34198fe9b83bfd90c500f4a762
grapefruit13 <liam.heo11@gmail.com>
2024-10-15 20:38:09 +0900

    Refactor: MealSearchContainer - 음식 검색 무한 스크롤을 위해 forwardRef 추가 및 프롭 수정

--- src/components/shared/Meal/MealSearchContainer/index.tsx
+++ src/components/shared/Meal/MealSearchContainer/index.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent } from 'react';
+import { ChangeEvent, forwardRef } from 'react';
 import { FoodInfo } from '@/type/menu/menuResponse';
 import { Input } from '@/components/common/Input';
 import { NutritionMenu } from '@/components/common/Typography';
@@ -10,39 +10,69 @@ type MealSearchContainerProps = {
   onChange: (e: ChangeEvent<HTMLInputElement>) => void;
   onSubmit: () => void;
   onClickNewMenu: (menu: string) => void;
+  isError?: boolean;
+  isLoading: boolean;
+  hasMore: boolean;
+  onScroll: () => void;
 };
 
-const MealSearchContainer = ({
-  keyword,
-  searchResultList,
-  onChange,
-  onSubmit,
-  onClickNewMenu,
-}: MealSearchContainerProps) => {
-  return (
-    <div className='flex w-full flex-col gap-2'>
-      <Input
-        className='text-md placeholder:text-md font-semibold'
-        placeholder='메뉴 이름을 입력해주세요'
-        bgcolor='meal'
-        includeButton
-        onChange={(e) => onChange(e)}
-        onSubmit={onSubmit}
-        value={keyword || ''}
-      />
-      <div className='scrollbar-gray-100 max-h-[380px] w-full overflow-y-auto rounded-md bg-white-200 p-2'>
-        {searchResultList.length === 0 ? (
-          <NutritionMenu>메뉴를 검색해주세요</NutritionMenu>
-        ) : (
-          <MealTable
-            data={searchResultList}
-            isButton
-            onClick={onClickNewMenu}
-          />
-        )}
+const MealSearchContainer = forwardRef<
+  HTMLDivElement,
+  MealSearchContainerProps
+>(
+  (
+    {
+      keyword,
+      searchResultList,
+      onChange,
+      onSubmit,
+      onClickNewMenu,
+      isError,
+      isLoading,
+      hasMore,
+      onScroll,
+    },
+    ref,
+  ) => {
+    return (
+      <div className='flex w-full flex-col gap-2'>
+        <Input
+          className='text-md placeholder:text-md font-semibold'
+          placeholder='메뉴 이름을 입력해주세요'
+          bgcolor='meal'
+          includeButton
+          onChange={(e) => onChange(e)}
+          onSubmit={onSubmit}
+          value={keyword || ''}
+        />
+        <div
+          className='scrollbar-gray-100 max-h-[380px] w-full overflow-y-auto rounded-md bg-white-200 p-2'
+          ref={ref}
+          onScroll={onScroll}
+        >
+          {searchResultList.length === 0 ? (
+            <NutritionMenu>
+              {isError ? '메뉴 검색에 실패했습니다' : '메뉴를 검색해주세요'}
+            </NutritionMenu>
+          ) : (
+            <>
+              <MealTable
+                data={searchResultList}
+                isButton
+                onClick={onClickNewMenu}
+              />
+              {isLoading && <NutritionMenu>로딩 중...</NutritionMenu>}
+              {!isLoading && !hasMore && (
+                <NutritionMenu>더 이상 결과가 없습니다.</NutritionMenu>
+              )}
+            </>
+          )}
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  },
+);
+
+MealSearchContainer.displayName = 'MealSearchContainer';
 
 export default MealSearchContainer;
