88a7f4f6df15e14ee32fb7c68ced4ce010ce6d42
grapefruit13 <liam.heo11@gmail.com>
2024-10-16 01:06:22 +0900

    Fix: 수동 식단 수정 페이지 - 식단 생성 페이지 식단 정보 연결, 식단 저장 API 연결

--- src/components/feature/MenualPlanEdit/index.tsx
+++ src/components/feature/MenualPlanEdit/index.tsx
@@ -1,40 +1,56 @@
 'use client';
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { zodResolver } from '@hookform/resolvers/zod';
+import { useQueryClient } from '@tanstack/react-query';
+import { AxiosError } from 'axios';
 import { useForm } from 'react-hook-form';
 import { mealHeaderSchema } from '@/schema/mealSchema';
+import { CalendarInfo } from '@/type/mealType';
+import { MajorCategory } from '@/type/menu/menuRequest';
 import { FoodInfo } from '@/type/menu/menuResponse';
 import {
   HandleChangeCategoryParam,
   SelectedCategory,
 } from '@/type/menuCategory/category';
-import { isValidDateString } from '@/utils/calendar';
+import { FailResponse, Result } from '@/type/response';
+import {
+  getCurrentYearMonthNow,
+  isValidDateString,
+  transformCalendarToPostSave,
+} from '@/utils/calendar';
 import MealForm from '@/components/common/MealForm';
+import { Option } from '@/components/common/Selectbox';
 import MealCalendar from '@/components/shared/Meal/MealCalender';
 import MealHeader from '@/components/shared/Meal/MealHeader';
-import { MOCK_NEW_CALENDAR_NUTRITION } from '@/constants/_calendarData';
-import { ORGANIZATIONS } from '@/constants/_getAllList/_categories';
+import { MAJOR_CATEGORIES } from '@/constants/_meal';
 import { MEAL_FORM_LEGEND } from '@/constants/_MealForm';
+import { ROUTES } from '@/constants/_navbar';
 import { PAGE_TITLE } from '@/constants/_pageTitle';
 import { MEAL_HEADER_ERROR } from '@/constants/_schema';
+import { usePostMonthMenusSave } from '@/hooks/menu/usePostMonthMenusSave';
+import useNavigate from '@/hooks/useNavigate';
+import { useMenualPlanStore } from '@/stores/useMenualPlanStore';
 import { useToastStore } from '@/stores/useToastStore';
 
 const MenualPlanEdit = () => {
-  // api로부터 전달 받는 값
-  const mealName = '맛있는 9월 식단 야호';
-  const currentYear = 2024;
-  const currentMonth = 9;
-
-  const [totalMenuList, setTotalMenuList] = useState(
-    MOCK_NEW_CALENDAR_NUTRITION,
-  );
+  const { monthMenuName, category, calendar } = useMenualPlanStore((state) => ({
+    monthMenuName: state.monthMenuName,
+    category: state.category,
+    calendar: state.calendar,
+  }));
+  const [calendarData, setCalendarData] = useState<CalendarInfo>(calendar);
   const [selectedDate, setSelectedDate] = useState<string>('');
-  const [selectedCategory, setSelectedCategory] = useState<SelectedCategory>({
-    majorCategory: '',
-    minorCategory: '',
-  });
+  const [selectedCategory, setSelectedCategory] =
+    useState<SelectedCategory>(category);
+  const { year, month } = getCurrentYearMonthNow();
+  const [isCategoryError, setIsCategoryError] = useState(false);
+  const [minorCategories, setMinorCategories] = useState<Option[]>([]);
   const showToast = useToastStore((state) => state.showToast);
+  const { navigate } = useNavigate();
+
+  const queryClient = useQueryClient();
+  const { mutate: postSaveMutate } = usePostMonthMenusSave();
 
   const {
     register,
@@ -44,7 +60,7 @@ const MenualPlanEdit = () => {
     resolver: zodResolver(mealHeaderSchema),
     mode: 'onChange',
     defaultValues: {
-      monthMenuName: mealName,
+      monthMenuName,
     },
   });
 
@@ -53,7 +69,7 @@ const MenualPlanEdit = () => {
     menuName: string,
     updatedItem: FoodInfo,
   ) => {
-    setTotalMenuList((prevList) => {
+    setCalendarData((prevList) => {
       if (!prevList[date]) {
         return prevList;
       }
@@ -62,9 +78,8 @@ const MenualPlanEdit = () => {
         ...prevList,
         [date]: {
           ...prevList[date],
-          foods: prevList[date].foods.map(
-            (item) =>
-              item.foodName === menuName ? { ...item, ...updatedItem } : item, // 업데이트할 아이템만 수정
+          foods: prevList[date].foods.map((item) =>
+            item.foodName === menuName ? { ...item, ...updatedItem } : item,
           ),
         },
       };
@@ -79,6 +94,7 @@ const MenualPlanEdit = () => {
       ...prev,
       [type]: value,
     }));
+    setIsCategoryError(false);
   };
 
   const handleDateClick = (date: string) => {
@@ -87,13 +103,39 @@ const MenualPlanEdit = () => {
 
   const handleResetMenu = () => {
     // 메뉴 초기화 클릭 시 실행
-    setTotalMenuList(MOCK_NEW_CALENDAR_NUTRITION);
+    setCalendarData(calendar);
     setSelectedDate('');
   };
 
   const onSubmit = (data: { monthMenuName: string }) => {
-    // 선택한 카테고리 + 식단이름 제출 + 식단 리스트로 이동
-    console.log(data);
+    const { majorCategory, minorCategory } = selectedCategory;
+
+    const isSelectedCategoryInvalid =
+      majorCategory === '' || minorCategory === '';
+
+    if (isSelectedCategoryInvalid) {
+      showToast(MEAL_HEADER_ERROR.category.min, 'warning', 3000);
+      setIsCategoryError(true);
+      return;
+    }
+
+    const formattedData = transformCalendarToPostSave(
+      calendarData,
+      data.monthMenuName,
+      selectedCategory.majorCategory as MajorCategory,
+      selectedCategory.minorCategory,
+    );
+    postSaveMutate(formattedData, {
+      onSuccess: ({ message }: Result<null>) => {
+        showToast(message, 'success', 1000);
+        navigate(ROUTES.VIEW.PLAN);
+      },
+      onError: (error: AxiosError<FailResponse>) => {
+        const errorMessage =
+          error?.response?.data?.message || '자동 식단 저장 실패';
+        showToast(errorMessage, 'warning', 1000);
+      },
+    });
   };
 
   const onError = () => {
@@ -103,24 +145,56 @@ const MenualPlanEdit = () => {
     }
   };
 
+  useEffect(() => {
+    const fetchCategories = () => {
+      switch (selectedCategory.majorCategory) {
+        case MAJOR_CATEGORIES[0]:
+          return queryClient.getQueryData<Result<string[]>>([
+            'getSchoolMinorCategories',
+          ]);
+        case MAJOR_CATEGORIES[1]:
+          return queryClient.getQueryData<Result<string[]>>([
+            'getSchoolNameMinorCategories',
+          ]);
+        case MAJOR_CATEGORIES[2]:
+          return queryClient.getQueryData<Result<string[]>>([
+            'getHospitalMinorCategories',
+          ]);
+        default:
+          return null;
+      }
+    };
+
+    const categories = fetchCategories();
+    if (!categories) return;
+    const { data } = categories;
+    if (!data) return;
+    const formattedData = data.map((category) => ({
+      value: category,
+      label: category,
+    }));
+    setMinorCategories(formattedData);
+  }, [selectedCategory.majorCategory]);
+
   return (
     <MealForm
       legend={MEAL_FORM_LEGEND.menual.edit}
       handleSubmit={handleSubmit(onSubmit, onError)}
     >
       <MealHeader
-        categories={ORGANIZATIONS}
+        categories={minorCategories}
         register={register}
         errors={errors}
         selectedCategory={selectedCategory}
         handleChangeCategory={handleChangeCategory}
+        isCategoryError={isCategoryError}
         pageHeaderTitle={PAGE_TITLE.autoPlan.edit}
       />
       <MealCalendar
         type='edit'
-        year={currentYear}
-        month={currentMonth}
-        data={totalMenuList}
+        year={year}
+        month={month}
+        data={calendarData}
         onDateClick={handleDateClick}
         selectedDate={selectedDate}
         handleChangeMenu={handleChangeMenu}
