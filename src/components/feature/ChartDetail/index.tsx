5bebc1aa7905705b81305831d56e87de9f0a8e61
plla2 <jhbljs92@naver.com>
2024-10-15 13:09:21 +0900

    Feat: 설문 상세페이지에서 질문수정페이지로 라우팅

--- src/components/feature/ChartDetail/index.tsx
+++ src/components/feature/ChartDetail/index.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import Image from 'next/image';
+import Link from 'next/link';
 import { useRouter } from 'next/navigation';
 import { useQueryClient } from '@tanstack/react-query';
 import { AxiosError } from 'axios';
