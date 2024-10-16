58aeb5e27c682b9e14581a52b6a5be64c151e0f3
plla2 <jhbljs92@naver.com>
2024-10-16 13:25:55 +0900

    Fix: 토큰저장 유틸함수 수정

--- src/utils/saveTokens.ts
+++ src/utils/saveTokens.ts
@@ -1,11 +1,13 @@
 import { setCookie } from 'nookies';
 import { ReissueResponse } from '@/type/auth/authResponse';
+import { destroyTokens } from './destroyTokens';
 
 const COOKIE_MAX_AGE = 60 * 60 * 24 * 4;
 
 export const saveTokens = (response: ReissueResponse) => {
   if (typeof window === 'undefined') return;
   const { accessToken, refreshToken } = response;
+  destroyTokens();
 
   localStorage.setItem('accessToken', accessToken);
 
