3cbfa2c9e07e21109708c4e50b704dcf9a0b30a4
grapefruit13 <liam.heo11@gmail.com>
2024-10-17 22:08:25 +0900

    Fix: MealCreate, MealEdit - page 초기값 0으로 수정(api 세팅)

--- src/components/shared/Meal/MealEdit/index.tsx
+++ src/components/shared/Meal/MealEdit/index.tsx
@@ -28,7 +28,7 @@ const MealEdit = ({ date, data, handleChangeMenu }: MealEditProps) => {
   const [keyword, setKeyword] = useState('');
   const [isSearchShow, setIsSearchShow] = useState(false);
   const [searchResultList, setSearchResultList] = useState<FoodInfo[]>([]);
-  const [page, setPage] = useState(1);
+  const [page, setPage] = useState(0);
   const [hasMore, setHasMore] = useState(true);
   const showToast = useToastStore((state) => state.showToast);
 
