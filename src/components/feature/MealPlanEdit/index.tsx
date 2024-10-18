65792f0f43632784a721cd14462f993198cceb7f
grapefruit13 <liam.heo11@gmail.com>
2024-10-18 21:24:55 +0900

    Feat: 식단 수정 훅 구현 및 식단 수정 페이지 API 연결

--- src/components/feature/MealPlanEdit/index.tsx
+++ src/components/feature/MealPlanEdit/index.tsx
@@ -2,39 +2,62 @@
 
 import { useState } from 'react';
 import { zodResolver } from '@hookform/resolvers/zod';
+import { useQueryClient } from '@tanstack/react-query';
+import { AxiosError } from 'axios';
 import { useForm } from 'react-hook-form';
 import { mealHeaderSchema } from '@/schema/mealSchema';
-import { FoodInfo } from '@/type/menu/menuResponse';
+import { MajorCategory } from '@/type/menu/menuRequest';
+import { FoodInfo, MenuResponseDTO } from '@/type/menu/menuResponse';
 import {
   HandleChangeCategoryParam,
   SelectedCategory,
 } from '@/type/menuCategory/category';
-import { isValidDateString } from '@/utils/calendar';
+import { FailResponse, Result } from '@/type/response';
+import {
+  isValidDateString,
+  transformCalendarToPostSave,
+} from '@/utils/calendar';
 import MealForm from '@/components/common/MealForm';
 import MealCalendar from '@/components/shared/Meal/MealCalender';
-import MealHeader from '@/components/shared/Meal/MealHeader';
-import { MOCK_NEW_CALENDAR_NUTRITION } from '@/constants/_calendarData';
-import { ORGANIZATION_LIST } from '@/constants/_category';
+import MealHeader, {
+  MealHeaderFormData,
+} from '@/components/shared/Meal/MealHeader';
 import { MEAL_FORM_LEGEND } from '@/constants/_MealForm';
+import { ROUTES } from '@/constants/_navbar';
 import { PAGE_TITLE } from '@/constants/_pageTitle';
 import { MEAL_HEADER_ERROR } from '@/constants/_schema';
+import { usePutMonthMenus } from '@/hooks/menu/usePutMonthMenus';
+import { useFetchMinorCategories } from '@/hooks/menuCategory/useFetchMinorCategories';
+import useNavigate from '@/hooks/useNavigate';
+import { useMealPlanStore } from '@/stores/useMealPlanStore';
 import { useToastStore } from '@/stores/useToastStore';
 
-const MealPlanEdit = () => {
-  // api로부터 전달 받는 값
-  const mealName = '맛있는 9월 식단 야호';
-  const currentYear = 2024;
-  const currentMonth = 9;
+type MealPlanEditProps = {
+  id: string;
+};
 
-  const [totalMenuList, setTotalMenuList] = useState(
-    MOCK_NEW_CALENDAR_NUTRITION,
+// TODO: 식단 수정 API 연결
+const MealPlanEdit = ({ id: monthMenuId }: MealPlanEditProps) => {
+  const { monthMenuName, category, calendar, year, month } = useMealPlanStore(
+    (state) => ({
+      monthMenuName: state.monthMenuName,
+      category: state.category,
+      calendar: state.calendar,
+      year: state.year,
+      month: state.month,
+    }),
   );
+  const [calendarData, setCalendarData] = useState(calendar);
   const [selectedDate, setSelectedDate] = useState<string>('');
-  const [selectedCategory, setSelectedCategory] = useState<SelectedCategory>({
-    majorCategory: '',
-    minorCategory: '',
-  });
+  const [selectedCategory, setSelectedCategory] =
+    useState<SelectedCategory>(category);
+  const [isCategoryError, setIsCategoryError] = useState(false);
   const showToast = useToastStore((state) => state.showToast);
+  const { navigate } = useNavigate();
+
+  const queryClient = useQueryClient();
+  const { minorCategories } = useFetchMinorCategories(category.majorCategory);
+  const { mutate: putMutate } = usePutMonthMenus();
 
   const {
     register,
@@ -44,7 +67,7 @@ const MealPlanEdit = () => {
     resolver: zodResolver(mealHeaderSchema),
     mode: 'onChange',
     defaultValues: {
-      monthMenuName: mealName,
+      monthMenuName,
     },
   });
 
@@ -53,7 +76,7 @@ const MealPlanEdit = () => {
     menuName: string,
     updatedItem: FoodInfo,
   ) => {
-    setTotalMenuList((prevList) => {
+    setCalendarData((prevList) => {
       if (!prevList[date]) {
         return prevList;
       }
@@ -86,13 +109,49 @@ const MealPlanEdit = () => {
 
   const handleResetMenu = () => {
     // 메뉴 초기화 클릭 시 실행
-    setTotalMenuList(MOCK_NEW_CALENDAR_NUTRITION);
+    setCalendarData(calendar);
     setSelectedDate('');
   };
 
-  const onSubmit = (data: { monthMenuName: string }) => {
-    // 선택한 카테고리 + 식단이름 제출 + 식단 리스트로 이동
-    console.log(data);
+  const onSubmit = (data: MealHeaderFormData) => {
+    const { majorCategory, minorCategory } = selectedCategory;
+
+    const isSelectedCategoryInvalid =
+      majorCategory === '' || minorCategory === '';
+
+    if (isSelectedCategoryInvalid) {
+      showToast(MEAL_HEADER_ERROR.category.min, 'warning', 3000);
+      setIsCategoryError(true);
+      return;
+    }
+
+    const formattedData = transformCalendarToPostSave(
+      calendar,
+      data.monthMenuName,
+      selectedCategory.majorCategory as MajorCategory,
+      selectedCategory.minorCategory,
+      calendarData,
+    );
+    putMutate(
+      {
+        request: formattedData,
+        monthMenuId,
+      },
+      {
+        onSuccess: ({ message }: Result<MenuResponseDTO>, variables) => {
+          showToast(message, 'success', 1000);
+          queryClient.invalidateQueries({
+            queryKey: ['monthMenuDetail', variables.monthMenuId],
+          });
+          navigate(`${ROUTES.VIEW.PLAN}/${monthMenuId}`);
+        },
+        onError: (error: AxiosError<FailResponse>) => {
+          const errorMessage =
+            error?.response?.data?.message || '식단 수정 실패';
+          showToast(errorMessage, 'warning', 1000);
+        },
+      },
+    );
   };
 
   const onError = () => {
@@ -108,19 +167,19 @@ const MealPlanEdit = () => {
       handleSubmit={handleSubmit(onSubmit, onError)}
     >
       <MealHeader
-        // TODO: 실 카테고리로 변경
-        categories={ORGANIZATION_LIST}
+        categories={minorCategories}
         register={register}
         errors={errors}
         selectedCategory={selectedCategory}
         handleChangeCategory={handleChangeCategory}
+        isCategoryError={isCategoryError}
         pageHeaderTitle={PAGE_TITLE.mealPlan.edit}
       />
       <MealCalendar
         type='edit'
-        year={currentYear}
-        month={currentMonth}
-        data={totalMenuList}
+        year={year}
+        month={month}
+        data={calendarData}
         onDateClick={handleDateClick}
         selectedDate={selectedDate}
         handleChangeMenu={handleChangeMenu}
