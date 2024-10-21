39eb728d44d4c292a81373dd960af55858614d22
plla2 <jhbljs92@naver.com>
2024-10-21 22:52:33 +0900

    Fix: rebase 수정

--- src/components/feature/ChartDetail/index.tsx
+++ src/components/feature/ChartDetail/index.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import Image from 'next/image';
-import Link from 'next/link';
 import { useRouter } from 'next/navigation';
 import { useQueryClient } from '@tanstack/react-query';
 import { AxiosError } from 'axios';
