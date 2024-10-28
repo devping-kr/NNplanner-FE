c24eb81d9dc0be36b43062d153a31a06357ede94
grapefruit13 <liam.heo11@gmail.com>
2024-10-28 14:04:26 +0900

    Fix: AutoPlanStore - 카테고리 추가 저장할 수 있게 수정

--- src/stores/useAutoPlanStore.ts
+++ src/stores/useAutoPlanStore.ts
@@ -1,11 +1,22 @@
 import { create } from 'zustand';
+import { SelectedCategory } from '@/type/menuCategory/category';
 
 type AutoPlanState = {
   monthMenuName: string;
+  category: SelectedCategory;
   setMonthMenuName: (name: string) => void;
+  setCategory: (category: SelectedCategory) => void;
 };
 
 export const useAutoPlanStore = create<AutoPlanState>((set) => ({
   monthMenuName: '',
-  setMonthMenuName: (name) => set({ monthMenuName: name }),
+  category: {
+    majorCategory: '학교',
+    minorCategory: '',
+  },
+  setMonthMenuName: (name: string) => set({ monthMenuName: name }),
+  setCategory: (category: SelectedCategory) =>
+    set(() => ({
+      category: category,
+    })),
 }));
