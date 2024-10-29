6638ba487ecec88ef46f563559d9ca26b8f7910f
grapefruit13 <liam.heo11@gmail.com>
2024-10-29 20:38:30 +0900

    Chore: next config image remote patterns 설정

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'www.nongsaro.go.kr',
+        port: '',
+        pathname: '/**/**/*',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
