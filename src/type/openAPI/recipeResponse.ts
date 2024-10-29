a1bfc6e2fbf453520f1b0982b68ff2deba0e8e4d
grapefruit13 <liam.heo11@gmail.com>
2024-10-29 20:34:01 +0900

    Feat: 월별 레시피 조회 query hook 구현

---
+++ src/type/openAPI/recipeResponse.ts
@@ -0,0 +1,10 @@
+export interface MenuRecipeListResponse {
+  recipeId: string;
+  month: number;
+  recipeName: string;
+  mainIngredient: string;
+  subIngredient: string;
+  instructions: string;
+  forGroup: string;
+  imageUrl: string;
+}
