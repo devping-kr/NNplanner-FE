ef495c01667ca32254b1b0b1759d492c86252e5b
grapefruit13 <liam.heo11@gmail.com>
2024-11-01 00:17:27 +0900

    Fix: 식단 검색 함수 페이지네이션 제외 파람들 null이면 파람에서 제외되게 설정

--- src/api/meal/index.ts
+++ src/api/meal/index.ts
@@ -5,32 +5,43 @@ import {
 } from '@/type/menu/menuRequest';
 import { MonthMenusResponse } from '@/type/menu/menuResponse';
 import { Result } from '@/type/response';
-import { BASE_API } from '@/constants/_apiPath';
+import { MENUS_API } from '@/constants/_apiPath';
+
+const { MONTH_MENUS, SEARCH } = MENUS_API;
 
 const getMealList = async ({
   size = 8,
   page,
   sort = 'createdAt,desc',
 }: GetMealListReqeust) => {
-  const response = await get<Result<MonthMenusResponse>>(BASE_API.MONTH_MENUS, {
+  const response = await get<Result<MonthMenusResponse>>(MONTH_MENUS, {
     params: { page, sort, size },
   });
   return response.data;
 };
 
 const getSearchMealList = async ({
+  majorCategory,
+  minorCategory,
+  menuName,
+  year,
+  month,
   size = 8,
   page,
   sort = 'createdAt,desc',
-  majorCategory,
-  minorCategory,
 }: GetSearchMealListRequest) => {
-  const response = await get<Result<MonthMenusResponse>>(
-    `${BASE_API.MENU_CATEGORIES}${BASE_API.MONTH_MENUS}?major-category=${majorCategory}&minor-category=${minorCategory}`,
-    {
-      params: { page, sort, size },
+  const response = await get<Result<MonthMenusResponse>>(SEARCH, {
+    params: {
+      ...(majorCategory != null && { majorCategory }),
+      ...(minorCategory != null && { minorCategory }),
+      ...(menuName != null && { menuName }),
+      ...(year != null && { year }),
+      ...(month != null && { month }),
+      page,
+      sort,
+      size,
     },
-  );
+  });
   return response.data;
 };
 
