7ff107278df0d6445f6207e7a79994e31693d28b
grapefruit <liam.heo11@gmail.com>
2024-12-30 13:27:43 +0900

    chore: GA_ID env 추가

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -2,4 +2,5 @@ export const env = Object.freeze({
   BASE_API_URL: `${process.env.NEXT_PUBLIC_BASE_API_URL}`,
   QR_API_URL: `${process.env.NEXT_PUBLIC_QR_API_URL}`,
   QR_APP_KEY: `${process.env.NEXT_PUBLIC_QR_APPKEY}`,
+  GA_ID: process.env.NEXT_PUBLIC_GA_ID,
 });
