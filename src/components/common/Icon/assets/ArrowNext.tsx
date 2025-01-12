804dfccf40f8f4d4fa677c90413bd161d6699ef4
grapefruit <liam.heo11@gmail.com>
2025-01-12 22:37:52 +0900

    fix: arrowNext 아이콘 변경

--- src/components/common/Icon/assets/ArrowNext.tsx
+++ src/components/common/Icon/assets/ArrowNext.tsx
@@ -11,15 +11,16 @@ const ArrowNext = ({
       width={width}
       height={height}
       viewBox='0 0 24 24'
+      fill='none'
       xmlns='http://www.w3.org/2000/svg'
       {...props}
     >
       <path
-        fillRule='evenodd'
-        clipRule='evenodd'
-        d='M9.86321 6.2636C10.2147 5.91213 10.7845 5.91213 11.136 6.2636L16.236 11.3636C16.5875 11.7151 16.5875 12.2849 16.236 12.6364L11.136 17.7364C10.7845 18.0879 10.2147 18.0879 9.86321 17.7364C9.51174 17.3849 9.51174 16.8151 9.86321 16.4636L14.3268 12L9.86321 7.5364C9.51174 7.18492 9.51174 6.61508 9.86321 6.2636Z'
-        stroke='none'
-        fill={color}
+        d='M8.25 4.5L15.75 12L8.25 19.5'
+        stroke={color}
+        strokeWidth='1.5'
+        strokeLinecap='round'
+        strokeLinejoin='round'
       />
     </svg>
   );
