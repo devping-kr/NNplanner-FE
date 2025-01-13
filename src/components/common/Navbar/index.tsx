b95ead59ab734ad8f0d90da557844bab5db7c99e
grapefruit <liam.heo11@gmail.com>
2025-01-13 11:17:11 +0900

    fix: Navbar - NavbarSection 컴포넌트 적용

--- src/components/common/Navbar/index.tsx
+++ src/components/common/Navbar/index.tsx
@@ -1,9 +1,7 @@
 'use client';
 
 import { usePathname } from 'next/navigation';
-import Icon from '@/components/common/Icon';
-import NavMenu from '@/components/common/NavMenu';
-import { NavbarTypo } from '@/components/common/Typography';
+import NavbarSection from '@/components/shared/Nav/NavbarSection';
 import { NAV_LINKS } from '@/constants/_navbar';
 
 const Navbar = () => {
@@ -15,41 +13,20 @@ const Navbar = () => {
   const isSurveyPage = /^\/survey\/\d+$/.test(pathname);
   const mealLinks = NAV_LINKS.slice(0, 4);
   const surveyLinks = NAV_LINKS.slice(4);
-  const ICON_SIZE = 24;
 
   return (
     !isSurveyPage && (
       <nav className='sticky top-0 flex h-screen w-[260px] min-w-[260px] flex-col bg-grey-800 pt-[88px]'>
-        <NavbarTypo className='px-6 pb-4 pt-6'>식단</NavbarTypo>
-        <div className='flex flex-col'>
-          {mealLinks.map(({ name, href, icon }) => (
-            <NavMenu key={name} href={href} isActive={selectedTab === name}>
-              <Icon
-                name={icon}
-                className='hover:stroke-green-500'
-                color={selectedTab === name ? 'green500' : 'grey500'}
-                width={ICON_SIZE}
-                height={ICON_SIZE}
-              />
-              <span>{name}</span>
-            </NavMenu>
-          ))}
-        </div>
-        <NavbarTypo className='px-6 pb-4 pt-6'>설문</NavbarTypo>
-        <div className='flex flex-col'>
-          {surveyLinks.map(({ name, href, icon }) => (
-            <NavMenu key={name} href={href} isActive={selectedTab === name}>
-              <Icon
-                name={icon}
-                className='hover:stroke-green-500'
-                color={selectedTab === name ? 'green500' : 'grey500'}
-                width={ICON_SIZE}
-                height={ICON_SIZE}
-              />
-              <span>{name}</span>
-            </NavMenu>
-          ))}
-        </div>
+        <NavbarSection
+          title='식단'
+          links={mealLinks}
+          selectedTab={selectedTab}
+        />
+        <NavbarSection
+          title='설문'
+          links={surveyLinks}
+          selectedTab={selectedTab}
+        />
       </nav>
     )
   );
