8d8843043e973edeb5fbee96ef3f758f8ed4ddb8
grapefruit <liam.heo11@gmail.com>
2025-01-13 11:16:07 +0900

    style: NavMenu variants 스타일 수정

--- src/components/common/NavMenu/NavMenu.variant.tsx
+++ src/components/common/NavMenu/NavMenu.variant.tsx
@@ -1,12 +1,12 @@
 import { cva } from 'class-variance-authority';
 
 export const navMenuVariants = cva(
-  'flex items-center gap-4 w-full px-6 py-4 bg-transparent h-14 cursor-pointer hover:bg-grey-900 active:bg-grey-900',
+  'flex items-center gap-4 w-full px-6 py-4 bg-transparent h-14 cursor-pointer active:bg-grey-900',
   {
     variants: {
       isActive: {
-        true: 'bg-grey-900 text-green-500 ',
-        false: 'text-gray-600',
+        true: 'bg-grey-900 text-green-500',
+        false: 'text-gray-500',
       },
     },
   },
