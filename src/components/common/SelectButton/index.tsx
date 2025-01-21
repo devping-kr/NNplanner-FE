e517249413cffc75d03d93894258754346081c4a
grapefruit <liam.heo11@gmail.com>
2025-01-21 22:19:54 +0900

    feat: selectButton 리디자인 적용- buttonSize, bgColor 프롭 추가

--- src/components/common/SelectButton/index.tsx
+++ src/components/common/SelectButton/index.tsx
@@ -1,48 +1,62 @@
 import { VariantProps } from 'class-variance-authority';
 import { cn } from '@/utils/core';
-import { selectIconVariants } from '../Selectbox/Selectbox.variant';
 import Icon from '@/components/common/Icon';
-import { Size } from '@/components/common/Selectbox';
-import { selectButtonVariants } from '@/components/common/SelectButton/SelectButton.variant';
+import { BgColor, ButtonSize, Size } from '@/components/common/Selectbox';
+import { selectIconVariants } from '@/components/common/Selectbox/Selectbox.variant';
+import {
+  selectButtonVariants,
+  typoVariants,
+} from '@/components/common/SelectButton/SelectButton.variant';
 
 type SelectButtonProps = VariantProps<typeof selectButtonVariants> & {
   selectedOption: string | null;
   placeholder: string;
-  size: Size;
+  buttonSize: ButtonSize;
+  bgColor: BgColor;
   isOpen: boolean;
   className?: string;
-  onClick: () => void;
   isError?: boolean;
-};
-
-const ICON_SIZE: Record<Size, number> = {
-  small: 16,
-  basic: 16,
-  large: 20,
+  onClick: () => void;
+  // 리디자인 완성 시 삭제
+  size?: Size;
 };
 
 const SelectButton = ({
   selectedOption,
   placeholder,
-  size,
+  buttonSize,
+  bgColor,
   isOpen,
   className,
-  onClick,
   isError = false,
+  onClick,
+  size,
 }: SelectButtonProps) => {
-  const iconSize = ICON_SIZE[size];
+  const iconSize = buttonSize === 'sm' ? 20 : 24;
+
   return (
     <button
-      className={cn(selectButtonVariants({ size, isError }), className)}
+      className={cn(
+        selectButtonVariants({ size, buttonSize, bgColor, isError }),
+        className,
+      )}
       type='button'
       onClick={onClick}
       aria-haspopup='listbox'
       aria-expanded={isOpen}
     >
-      <span>{selectedOption || placeholder}</span>
+      <span
+        className={typoVariants({
+          buttonSize,
+          state: selectedOption ? 'selected' : 'placeholder',
+        })}
+      >
+        {selectedOption || placeholder}
+      </span>
       <Icon
         className={selectIconVariants({ size })}
         name={isOpen ? 'arrowUp' : 'arrowDown'}
+        color='black'
         width={iconSize}
         height={iconSize}
       />
