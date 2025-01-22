e56fea746a50d24f5033d7031c27688e24789867
grapefruit <liam.heo11@gmail.com>
2025-01-22 18:50:08 +0900

    fix: optionList variants 공통 스타일 해당 컴포넌트에 직접 적용

--- src/components/common/OptionList/OptionList.variant.tsx
+++ src/components/common/OptionList/OptionList.variant.tsx
@@ -1,5 +1,6 @@
 import { cva } from 'class-variance-authority';
 
+// 추후 variants 비롯 파일 전체 삭제
 export const optionListUlVariants = cva('flex h-full w-full flex-col', {
   variants: {
     // 추후 삭제
@@ -11,16 +12,14 @@ export const optionListUlVariants = cva('flex h-full w-full flex-col', {
   },
 });
 
-export const optionListLiVariants = cva(
-  'cursor-pointer px-4 py-3 hover:bg-gray-50 active:bg-white-100 active:text-green-500',
-  {
-    variants: {
-      // 추후 삭제
-      size: {
-        small: '',
-        basic: '',
-        large: '',
-      },
+// 추후 variants 비롯 파일 전체 삭제
+export const optionListLiVariants = cva('', {
+  variants: {
+    // 추후 삭제
+    size: {
+      small: '',
+      basic: '',
+      large: '',
     },
   },
-);
+});
