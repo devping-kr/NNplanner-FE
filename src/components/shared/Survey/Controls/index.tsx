25409641590db3fb059a384d364dc2fa9aba8745
plla2 <jhbljs92@naver.com>
2025-01-23 14:01:40 +0900

    fix: DatepickerCalendar 컴포넌트 리디자인 적용 (calendar는 아직 미진행)

--- src/components/shared/Survey/Controls/index.tsx
+++ src/components/shared/Survey/Controls/index.tsx
@@ -1,11 +1,5 @@
-import { useRef } from 'react';
-import { ko } from 'date-fns/locale';
-import DatePicker from 'react-datepicker';
-import { getCurrentYearMonthNow } from '@/utils/calendar';
-import Icon from '@/components/common/Icon';
+import DatepickerCalendar from '@/components/common/DatepickerCalendar';
 import { Input } from '@/components/common/Input';
-import { Label } from '@/components/common/Typography';
-import CustomDatePickerHeader from '@/components/shared/Survey/CustomDatePickerHeader';
 
 interface Props {
   type: 'create' | 'edit';
@@ -26,15 +20,7 @@ const SurveyControls = ({
   deadLine,
   setDeadLine,
 }: Props) => {
-  const { now } = getCurrentYearMonthNow();
-  const deadLineDatePickerRef = useRef<DatePicker | null>(null);
   const isChangeable = type === 'create';
-
-  const handleChangeDate = (date: Date | null) => {
-    if (date) {
-      setDeadLine!(date);
-    }
-  };
 
   const handleChangeSurveyName = (e: React.ChangeEvent<HTMLInputElement>) => {
     if (e.target.value.length <= EXTRA_SURVEYNAME_LIMIT && !isChangeable) {
@@ -43,10 +29,6 @@ const SurveyControls = ({
     if (e.target.value.length <= EXTRA_SURVEYNAME_LIMIT && isChangeable) {
       setSurveyName!(e.target.value);
     }
-  };
-
-  const handleOpenDatePicker = () => {
-    deadLineDatePickerRef.current!.setFocus();
   };
 
   return (
@@ -61,36 +43,11 @@ const SurveyControls = ({
           height='basic'
         />
       </div>
-      <div className='relative flex w-56 items-center gap-2'>
-        <Label>마감 일자</Label>
-        <DatePicker
-          ref={deadLineDatePickerRef}
-          className='flex w-28 cursor-pointer border-b border-green-400 bg-transparent pl-1 focus:outline-none disabled:cursor-not-allowed disabled:opacity-80'
-          shouldCloseOnSelect
-          dateFormat='yyyy-MM-dd'
-          selected={deadLine}
-          locale={ko}
-          minDate={now}
-          onChange={handleChangeDate}
-          calendarClassName='custom-calendar'
-          dayClassName={() => 'custom-day'}
-          placeholderText='마감날짜 선택'
-          renderCustomHeader={CustomDatePickerHeader}
-        />
-        <button
-          onClick={handleOpenDatePicker}
-          disabled={!isChangeable}
-          className='disabled:cursor-not-allowed'
-        >
-          <Icon
-            name='calendar'
-            width={16}
-            height={16}
-            color='active'
-            className='absolute bottom-3 right-9'
-          />
-        </button>
-      </div>
+      <DatepickerCalendar
+        type={type}
+        deadLine={deadLine}
+        setDeadLine={setDeadLine}
+      />
     </div>
   );
 };
