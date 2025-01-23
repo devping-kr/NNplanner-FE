0f1e6d8a9226118493b52127f8399c315202c82c
grapefruit <liam.heo11@gmail.com>
2025-01-23 17:41:46 +0900

    feat: CalendarDay 리디자인

--- src/components/common/CalendarDay/index.tsx
+++ src/components/common/CalendarDay/index.tsx
@@ -1,5 +1,9 @@
 import { cn } from '@/utils/core';
-import { calendarDayVariants } from '@/components/common/Calendar/Calendar.variant';
+import { Body3Black, Subtitle2Black, Subtitle2Red500 } from '../Typography';
+import {
+  calendarDayButtonVariants,
+  calendarDayVariants,
+} from '@/components/common/Calendar/Calendar.variant';
 
 export type CalendarDayProps = {
   date: string;
@@ -7,11 +11,14 @@ export type CalendarDayProps = {
   isInvalid: boolean;
   isActive: boolean;
   data: { foodId: string; foodName: string }[];
+  index: number;
+  totalDays: number;
   readonly?: boolean;
   onClick: () => void;
 };
 
 export const MAXIUM_MENU_PER_DAY = 7;
+const DAY_COUNT_OF_A_WEEK = 7;
 
 const CalendarDay = ({
   date,
@@ -19,27 +26,45 @@ const CalendarDay = ({
   isInvalid,
   isActive,
   data,
+  index,
+  totalDays,
   readonly,
   onClick,
 }: CalendarDayProps) => {
+  const isLastRow =
+    Math.ceil((index + 1) / DAY_COUNT_OF_A_WEEK) ===
+    Math.ceil(totalDays / DAY_COUNT_OF_A_WEEK);
+
   return (
     <button
-      className={cn(
-        calendarDayVariants({ isInvalid, isActive, readonly }),
-        isHoliday ? 'text-red-500' : 'test-dark-100',
-      )}
       type='button'
       onClick={onClick}
+      className={cn(
+        calendarDayButtonVariants({ isInvalid, isActive, readonly }),
+        'border-r border-t border-grey-100',
+        index % DAY_COUNT_OF_A_WEEK === 0 && 'border-l',
+        isLastRow && 'border-b',
+      )}
     >
-      <div className={`text-left font-bold`}>{date}</div>
-      <div className='mt-[0.5] w-full text-dark-100'>
+      {isHoliday ? (
+        <Subtitle2Red500
+          className={(calendarDayVariants({ isInvalid }), 'bg-white-100')}
+        >
+          {date}
+        </Subtitle2Red500>
+      ) : (
+        <Subtitle2Black className={calendarDayVariants({ isInvalid })}>
+          {date}
+        </Subtitle2Black>
+      )}
+      <div className='w-full'>
         {data.slice(0, MAXIUM_MENU_PER_DAY).map((item, index) => (
-          <span
+          <Body3Black
             key={`${item.foodId}-${index}`}
-            className='block w-full truncate text-sm'
+            className='block w-full truncate text-nowrap'
           >
             {item.foodName}
-          </span>
+          </Body3Black>
         ))}
       </div>
     </button>
