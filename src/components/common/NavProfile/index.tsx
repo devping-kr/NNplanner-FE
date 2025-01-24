f5d8abbd28b99735c259cbdebe8b5fde81fa3a33
plla2 <jhbljs92@naver.com>
2025-01-24 14:12:29 +0900

    fix: 기존의 Badge 사용처 제거

--- src/components/common/NavProfile/index.tsx
+++ src/components/common/NavProfile/index.tsx
@@ -1,16 +1,15 @@
-import Badge from '@/components/common/Badge';
 import Icon from '@/components/common/Icon';
 import { Body2Black, Subtitle2Black } from '@/components/common/Typography';
 
 const NavProfile = ({ name = '냠냠' }: { name: string }) => {
   return (
     <button className='flex w-[238px] items-center justify-between gap-2'>
-      <Badge
-        // TODO: 추후 이미지 변경 필요
+      {/* TODO: 추후 컴포넌트 제작, 이미지 변경 필요 */}
+      {/* <Badge
         imageSrc='/imgs/pi-gon-ping.jpg'
         size={40}
         className='border border-grey-100'
-      />
+      /> */}
       <div className='flex items-center justify-center gap-2'>
         <Body2Black>다시 만나 기뻐요!</Body2Black>
         <Subtitle2Black>{name}님</Subtitle2Black>
