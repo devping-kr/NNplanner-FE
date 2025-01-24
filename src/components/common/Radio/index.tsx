bfd31baf3b98c7ac7e7c684919a8e7136b66e4af
plla2 <jhbljs92@naver.com>
2025-01-24 12:39:26 +0900

    style: 선택되지 않은 radio hover시 border스타일 추가

--- src/components/common/Radio/index.tsx
+++ src/components/common/Radio/index.tsx
@@ -20,8 +20,8 @@ const Radio = ({ option, question, answers, handleChange }: Props) => {
           onChange={() => handleChange(question.questionId, option)}
           className='peer sr-only'
         />
-        <span className='absolute h-5 w-5 rounded-full bg-white-100'></span>
-        <span className='absolute h-5 w-5 rounded-full border peer-checked:border-[6px] peer-checked:border-green-500'></span>
+        <span className='absolute h-5 w-5 rounded-full border border-grey-300 bg-white-100 peer-hover:border-2 peer-hover:border-grey-500'></span>
+        <span className='absolute h-5 w-5 rounded-full border-[6px] border-transparent peer-checked:border-green-500'></span>
         <span className='absolute h-5 w-5 rounded-full peer-checked:ring-green-500 peer-focus:ring-4 peer-focus:ring-green-100'></span>
       </label>
       <label htmlFor={`${question.questionId}_${option}`}>{option}</label>
