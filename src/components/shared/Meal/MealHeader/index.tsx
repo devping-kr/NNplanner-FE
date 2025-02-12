1a66ee93d2f07978534b746c4cdfc31cf53e3f84
grapefruit <liam.heo11@gmail.com>
2025-02-12 23:07:39 +0900

    refactor: MealHeader - props에 type과 각종 버튼 handler 추가

--- src/components/shared/Meal/MealHeader/index.tsx
+++ src/components/shared/Meal/MealHeader/index.tsx
@@ -1,11 +1,11 @@
 'use client';
 
 import { FieldErrors, UseFormRegister } from 'react-hook-form';
+import { CalendarType } from '@/type/calendar';
 import {
   HandleChangeCategoryParam,
   SelectedCategory,
 } from '@/type/menuCategory/category';
-import { cn } from '@/utils/core';
 import Button from '@/components/common/Button/Button';
 import { Input } from '@/components/common/Input';
 import { Option, Selectbox } from '@/components/common/Selectbox';
@@ -13,12 +13,14 @@ import { Caption1Grey500, H2BlackH2 } from '@/components/common/Typography';
 import { ORGANIZATION_LIST } from '@/constants/_category';
 import { AUTO_PLAN_BETA_MESSAGE } from '@/constants/_meal';
 import { PAGE_TITLE } from '@/constants/_pageTitle';
+import useNavigate from '@/hooks/useNavigate';
 
 export type MealHeaderFormData = {
   monthMenuName: string;
 };
 
 type MealHeaderProps = {
+  type?: CalendarType;
   pageHeaderTitle: string;
   categories: Option[];
   selectedCategory: SelectedCategory;
@@ -26,20 +28,34 @@ type MealHeaderProps = {
     type: HandleChangeCategoryParam,
     value: string,
   ) => void;
+  handleResetMenu?: () => void;
+  // handleSaveMenu?: (date: string, menuList: FoodInfo[]) => void;
+  handleEditMenu?: () => void;
+  handleCreateSurvey?: () => void;
+  handleSaveExcel?: () => void;
+  handleDeleteMenu?: () => void;
   register?: UseFormRegister<MealHeaderFormData>;
   errors?: FieldErrors<MealHeaderFormData>;
   isCategoryError?: boolean;
 };
 
 const MealHeader = ({
+  type = 'default',
   pageHeaderTitle,
   categories,
   selectedCategory,
   handleChangeCategory,
+  handleResetMenu,
+  // handleSaveMenu,
+  handleEditMenu,
+  handleCreateSurvey,
+  handleSaveExcel,
+  handleDeleteMenu,
   register,
   errors,
   isCategoryError,
 }: MealHeaderProps) => {
+  const { handleBack } = useNavigate();
   const isBothSelected =
     selectedCategory.majorCategory && selectedCategory.minorCategory;
 
@@ -55,10 +71,9 @@ const MealHeader = ({
         {register && errors && (
           <div className='relative flex h-fit flex-col'>
             <Input
-              bgcolor='meal'
+              variant='white'
               size='s'
               placeholder='식단명을 입력하세요'
-              className={cn('text-lg')}
               isError={!!errors.monthMenuName?.message}
               autoComplete='off'
               {...register('monthMenuName')}
@@ -69,7 +84,7 @@ const MealHeader = ({
         <div className='relative flex gap-4'>
           <Selectbox
             options={ORGANIZATION_LIST}
-            size='small'
+            buttonSize='sm'
             onChange={(majorCategory) => {
               handleChangeCategory('majorCategory', majorCategory);
             }}
@@ -92,14 +107,91 @@ const MealHeader = ({
               ),
           )}
         </div>
-        <Button
-          variant='primary'
-          size='sm'
-          type='submit'
-          disabled={!isBothSelected}
-        >
-          생성
-        </Button>
+
+        {type === 'default' && (
+          <Button
+            variant='primary'
+            size='sm'
+            type='submit'
+            disabled={!isBothSelected}
+          >
+            생성
+          </Button>
+        )}
+        {type === 'create' && (
+          <div className='flex w-fit items-center gap-2'>
+            <Button variant='primary' size='sm' type='submit' disabled={true}>
+              저장
+            </Button>
+            <Button
+              variant='teritary'
+              size='sm'
+              type='button'
+              onClick={handleEditMenu}
+            >
+              메뉴 수정
+            </Button>
+          </div>
+        )}
+        {type === 'edit' && (
+          <div className='flex w-fit items-center gap-2'>
+            <Button
+              size='sm'
+              variant='outline'
+              type='button'
+              onClick={handleResetMenu}
+            >
+              메뉴 초기화
+            </Button>
+            <Button size='sm' type='submit'>
+              수정 완료
+            </Button>
+            <Button size='sm' type='button' onClick={handleBack}>
+              취소
+            </Button>
+          </div>
+        )}
+        {type === 'menualCreate' && (
+          <div className='flex w-fit items-center gap-2'>
+            <Button
+              size='sm'
+              variant='outline'
+              type='button'
+              onClick={handleResetMenu}
+            >
+              메뉴 초기화
+            </Button>
+            <Button size='sm' type='submit'>
+              생성
+            </Button>
+          </div>
+        )}
+        {type === 'mealPlan' && (
+          <div className='flex w-fit items-center gap-2'>
+            <Button
+              size='sm'
+              variant='outline'
+              type='button'
+              onClick={handleCreateSurvey}
+            >
+              설문 생성
+            </Button>
+            <Button
+              size='sm'
+              variant='outline'
+              type='button'
+              onClick={handleSaveExcel}
+            >
+              엑셀 저장
+            </Button>
+            <Button size='sm' type='button' onClick={handleEditMenu}>
+              수정
+            </Button>
+            <Button size='sm' type='button' onClick={handleDeleteMenu}>
+              삭제
+            </Button>
+          </div>
+        )}
       </div>
     </div>
   );
