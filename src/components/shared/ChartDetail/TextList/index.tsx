b7cbdcf95598fe55446b8a444cb267d77cffa707
plla2 <jhbljs92@naver.com>
2025-02-13 10:23:55 +0900

    Feat: 기본 / 추가질문 컴포넌트 리디자인 적용

--- src/components/shared/ChartDetail/TextList/index.tsx
+++ src/components/shared/ChartDetail/TextList/index.tsx
@@ -1,5 +1,9 @@
 import { cn } from '@/utils/core';
-import { CardTitle, HeadPrimary } from '@/components/common/Typography';
+import {
+  Body2Black,
+  HeadPrimary,
+  SubTitle1Black,
+} from '@/components/common/Typography';
 
 interface Props {
   list: string[];
@@ -8,10 +12,8 @@ interface Props {
 }
 const TextList = ({ list, title, type }: Props) => {
   return (
-    <div className='flex w-full flex-col gap-3 rounded border border-gray-300 bg-white-100 p-5'>
-      <div className='border-b border-gray-200 pb-3'>
-        <CardTitle>{title}</CardTitle>
-      </div>
+    <div className='flex w-full flex-col gap-6 rounded-2xl bg-white-100 p-6'>
+      <SubTitle1Black>{title}</SubTitle1Black>
       <ul
         className={cn(
           'flex flex-col gap-2 overflow-y-auto pr-2',
@@ -19,7 +21,11 @@ const TextList = ({ list, title, type }: Props) => {
         )}
       >
         {list.length !== 0 &&
-          list.map((text, idx) => <li key={`${idx}-${text}`}>{text}</li>)}
+          list.map((text, idx) => (
+            <li key={`${idx}-${text}`} className='h-6'>
+              <Body2Black>{text}</Body2Black>
+            </li>
+          ))}
         {list.length === 0 && (
           <HeadPrimary>제출된 설문이 없습니다.</HeadPrimary>
         )}
--- src/components/shared/Survey/AdditionQuestions/index.tsx
+++ src/components/shared/Survey/AdditionQuestions/index.tsx
@@ -1,10 +1,8 @@
-import { useEffect, useRef, useState } from 'react';
-import { cn } from '@/utils/core';
+import { useState } from 'react';
 import Button from '@/components/common/Button/Button';
-import Icon from '@/components/common/Icon';
 import { Input } from '@/components/common/Input';
 import { Selectbox } from '@/components/common/Selectbox';
-import { CardTitle } from '@/components/common/Typography';
+import { CardTitle, SubTitle1Black } from '@/components/common/Typography';
 import { inputsType } from '@/components/feature/Survey/Create';
 import { WARNING } from '@/constants/_toastMessage';
 import { useToastStore } from '@/stores/useToastStore';
@@ -23,43 +21,40 @@ const ANSWER_TYPE = [
 ];
 
 const AdditionQuestions = ({ inputs, setInputs, successSubmit }: Props) => {
-  const [prevInputCount, setPrevInputCount] = useState(0);
-  const inputRef = useRef<HTMLInputElement>(null);
+  const [addInputValue, setAddInputValue] = useState('');
+  const [addInputType, setAddInputType] = useState('');
   const showToast = useToastStore((state) => state.showToast);
-
-  useEffect(() => {
-    if (inputs.length > prevInputCount && inputRef.current) {
-      inputRef.current?.focus();
-    }
-    setPrevInputCount(inputs.length);
-  }, [inputs]);
 
   const handleAddInput = () => {
     if (inputs.length >= EXTRA_QUESTIONS_LIMIT) {
       showToast(WARNING.maxAdditionQuestion, 'warning', 2000);
       return;
     }
+    if (addInputValue === '' || addInputType === '') {
+      showToast(WARNING.noQuestionValue, 'warning', 2000);
+      return;
+    }
     if (!successSubmit && inputs.length < EXTRA_QUESTIONS_LIMIT) {
       setInputs((prevInputs) => [
         ...prevInputs,
-        { question: '', answerType: 'text', questionId: Math.random() },
+        {
+          question: addInputValue,
+          answerType: addInputType,
+          questionId: Number(Math.random().toFixed(5)),
+        },
       ]);
+      setAddInputValue('');
+      setAddInputType('');
     }
   };
 
-  const handleChangeInput = (
-    e: React.ChangeEvent<HTMLInputElement>,
-    idx: number,
-  ) => {
-    const newInputs = [...inputs];
-    newInputs[idx].question = e.target.value;
-    setInputs(newInputs);
-  };
-
-  const handleChangeAnswerType = (type: string, idx: number) => {
-    const newInputs = [...inputs];
-    newInputs[idx].answerType = type;
-    setInputs(newInputs);
+  const handleChangeAddInputType = (selectedValue: string) => {
+    const selectedOption = ANSWER_TYPE.find(
+      (opt) => opt.value === selectedValue,
+    );
+    if (selectedOption) {
+      setAddInputType(selectedOption.value);
+    }
   };
 
   const handleDeleteInput = (indexToDelete: number) => {
@@ -69,54 +64,71 @@ const AdditionQuestions = ({ inputs, setInputs, successSubmit }: Props) => {
       );
   };
 
+  console.log(inputs);
+
   return (
-    <div className='flex w-full flex-col gap-5 rounded border border-gray-300 bg-white-100 p-5'>
-      <div className='flex items-center justify-between border-b border-gray-200 pb-2'>
-        <CardTitle>추가 질문</CardTitle>
-        <div className='w-24'>
-          <Button
-            onClick={handleAddInput}
+    <div className='flex w-full flex-col gap-6 rounded-2xl bg-white-100 p-6'>
+      <SubTitle1Black>추가 질문</SubTitle1Black>
+      <div className='flex items-center gap-2'>
+        <Input
+          type='text'
+          variant='grey50'
+          value={addInputValue}
+          onChange={(e) => setAddInputValue(e.target.value)}
+          placeholder='추가 질문을 입력해 주세요.'
+          borderRadius='large'
+        />
+        <div className='min-w-[110px]'>
+          <Selectbox
             size='small'
-            className={successSubmit ? 'cursor-default' : ''}
-            disabled={successSubmit}
-          >
-            <Icon name='plus' width={15} height={15} color='white' />
-            <span>질문 추가</span>
-          </Button>
+            options={ANSWER_TYPE}
+            selectedValue={addInputType}
+            placeholder='질문 형식'
+            onChange={handleChangeAddInputType}
+            buttonSize='sm'
+          />
         </div>
+        <Button
+          size='sm'
+          variant='teritary'
+          onClick={handleAddInput}
+          disabled={successSubmit}
+          className='rounded-lg'
+        >
+          등록
+        </Button>
       </div>
       <ul className='flex flex-col gap-3'>
         {inputs.length !== 0 ? (
           inputs.map((input, idx) => (
-            <li key={input.questionId} className='flex gap-2'>
+            <div key={`${input}-${idx}`} className='flex items-center gap-2'>
               <Input
                 type='text'
-                ref={idx === inputs.length - 1 ? inputRef : null}
-                bgcolor='meal'
+                variant='grey50'
                 value={input.question}
-                onChange={(e) => handleChangeInput(e, idx)}
-                placeholder='추가 질문을 입력해주세요.'
+                readOnly
+                borderRadius='large'
               />
-              <Selectbox
-                size='small'
-                options={ANSWER_TYPE}
-                selectedValue={
-                  input.answerType === 'text' ? '서술형' : '선택형'
-                }
-                placeholder='서술형'
-                onChange={(type) => handleChangeAnswerType(type, idx)}
-              />
-              <div className='flex w-24'>
-                <Button
+              <div className='min-w-[110px]'>
+                <Selectbox
                   size='small'
-                  variant={'outline'}
-                  onClick={() => handleDeleteInput(idx)}
-                  className={cn('w-24', successSubmit ? 'cursor-default' : '')}
-                >
-                  질문 삭제
-                </Button>
+                  selectedValue={
+                    input.answerType === 'text' ? '서술형' : '선택형'
+                  }
+                  readonly
+                  buttonSize='sm'
+                />
               </div>
-            </li>
+              <Button
+                size='sm'
+                variant='outline'
+                onClick={() => handleDeleteInput(idx)}
+                disabled={successSubmit}
+                className='rounded-lg'
+              >
+                삭제
+              </Button>
+            </div>
           ))
         ) : (
           <div className='text-center'>
