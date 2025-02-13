7c8e7a5c683e7c469e47894c17462fd0773defd2
plla2 <jhbljs92@naver.com>
2025-02-13 10:21:20 +0900

    Fix: root Layout 좌우 패딩값 설정

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -6,7 +6,7 @@ const RootLayout = ({ children }: { children: React.ReactNode }) => {
     <div className='flex min-h-screen'>
       <Header />
       <Navbar />
-      <main className='flex-grow bg-white-200 pt-[104px]'>{children}</main>
+      <main className='flex-grow bg-white-200 px-8 pt-[104px]'>{children}</main>
     </div>
   );
 };
