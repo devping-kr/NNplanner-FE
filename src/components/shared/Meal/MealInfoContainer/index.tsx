b08d859d19cd2863a003189a1d6f869fb7ca06ec
grapefruit <liam.heo11@gmail.com>
2025-02-16 22:25:12 +0900

    design: MealInfoContainer- 리디자인 스타일 적용

--- src/components/shared/Meal/MealInfoContainer/index.tsx
+++ src/components/shared/Meal/MealInfoContainer/index.tsx
@@ -7,7 +7,7 @@ type MealInfoContainerProps = {
 
 const MealInfoContainer = ({ children, date }: MealInfoContainerProps) => {
   return (
-    <div className='top-[246px] flex h-fit w-80 flex-col gap-6 rounded-2xl bg-white-100 p-4 pr-8'>
+    <div className='top-[246px] flex h-fit w-80 flex-col gap-6 rounded-2xl bg-white-100 p-4'>
       <Subtitle2Black>{date}</Subtitle2Black>
       {children}
     </div>
