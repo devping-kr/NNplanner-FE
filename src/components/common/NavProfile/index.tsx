67be838934afa298175c7bf51616d29a0d6f363f
plla2 <jhbljs92@naver.com>
2025-02-27 13:03:20 +0900

    Feat: Header의 profile 클릭시 마이페이지로 이동

--- src/components/common/NavProfile/index.tsx
+++ src/components/common/NavProfile/index.tsx
@@ -1,10 +1,22 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
 import Icon from '@/components/common/Icon';
 import ProfileImage from '@/components/common/ProfileImage';
 import { Body2Black, Subtitle2Black } from '@/components/common/Typography';
+import { ROUTES } from '@/constants/_navbar';
 
 const NavProfile = ({ name = '냠냠' }: { name: string }) => {
+  const router = useRouter();
+  const linkToMyPage = () => {
+    router.push(ROUTES.MYPAGE);
+  };
+
   return (
-    <button className='flex w-[238px] items-center justify-between gap-2'>
+    <button
+      className='flex w-[238px] items-center justify-between gap-2'
+      onClick={linkToMyPage}
+    >
       {/* TODO: 프로필 이미지 변경 필요 */}
       <ProfileImage src='/imgs/pi-gon-ping.jpg' size={40} />
       <div className='flex items-center justify-center gap-2'>
