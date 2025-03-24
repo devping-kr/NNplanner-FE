7b1a370b0603853bc617ec1c5e8f60f290cbbbba
plla2 <jhbljs92@naver.com>
2025-03-24 16:51:39 +0900

    Feat: 세번째 랜딩페이지 작업

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,15 @@
 import FirstContent from '@/components/feature/Landing/FirstContent';
 import LandingHeader from '@/components/feature/Landing/LandingHeader';
 import SecondContent from '@/components/feature/Landing/SecondContent';
+import ThirdContent from '@/components/feature/Landing/ThirdContent';
 
 export default function Home() {
   return (
-    <div className='flex flex-col'>
+    <div className='flex flex-col items-center'>
       <LandingHeader />
       <FirstContent />
       <SecondContent />
+      <ThirdContent />
     </div>
   );
 }
