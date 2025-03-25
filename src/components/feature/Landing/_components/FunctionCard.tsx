ff411cb9162b306ca2cadd12df820de09a8a6aa2
plla2 <jhbljs92@naver.com>
2025-03-25 09:45:07 +0900

    Feat: 네번째 랜딩페이지 카드 컴포넌트 작업

---
+++ src/components/feature/Landing/_components/FunctionCard.tsx
@@ -0,0 +1,39 @@
+import Image from 'next/image';
+import { Body2Grey800, H5Grey800 } from '@/components/common/Typography';
+
+interface Props {
+  imgHref: string;
+  firstContent: string;
+  secondContent: string;
+  title: string;
+}
+
+const FunctionCard = ({
+  imgHref,
+  firstContent,
+  secondContent,
+  title,
+}: Props) => {
+  return (
+    <div className='flex flex-col items-center gap-8'>
+      <div className='flex items-center justify-center rounded-2xl bg-white-100 px-[77px] py-14'>
+        <Image
+          src={imgHref}
+          alt='기능 이미지'
+          width={94}
+          height={104}
+          style={{ width: 94, height: 104 }}
+        />
+      </div>
+      <div className='flex flex-col items-center gap-4'>
+        <H5Grey800>{title}</H5Grey800>
+        <div className='flex flex-col items-center'>
+          <Body2Grey800>{firstContent}</Body2Grey800>
+          <Body2Grey800>{secondContent}</Body2Grey800>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FunctionCard;
