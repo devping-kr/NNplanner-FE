6cf72a0361277903a01784df49a9d855b5bac4a1
grapefruit <liam.heo11@gmail.com>
2025-03-25 13:43:00 +0900

    feat: 학교명 검색 api 함수 구현

--- src/api/menuCategory/index.ts
+++ src/api/menuCategory/index.ts
@@ -1,5 +1,6 @@
 import { get } from '@/lib/axios';
 import { MajorCategory } from '@/type/menu/menuRequest';
+import { GetSearchSchoolRequest } from '@/type/menuCategory/menuCategoryRequest';
 import { Result } from '@/type/response';
 import { MENU_CAGEGORY_API } from '@/constants/_apiPath';
 
@@ -14,6 +15,19 @@ const getMinorCategories = async (param: MajorCategory) => {
   return response.data;
 };
 
+/**
+ * @description 학교명 검색 api
+ */
+const getSearchSchool = async ({ keyword }: GetSearchSchoolRequest) => {
+  const response = await get<Result<string[]>>(MENU_CATEGORIES, {
+    params: {
+      keyword,
+    },
+  });
+  return response.data;
+};
+
 export const menuCategories = {
   getMinorCategories,
+  getSearchSchool,
 };
