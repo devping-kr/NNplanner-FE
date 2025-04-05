39172f2aaa5d6c90253cc2168ecf2676bc6c840e
grapefruit <liam.heo11@gmail.com>
2025-04-05 21:31:19 +0900

    feat: string array to option array 유틸 함수 구현 및 주석 추가

--- src/utils/meal.ts
+++ src/utils/meal.ts
@@ -1,10 +1,10 @@
+import { Option } from '@/components/common/Selectbox';
+
 /**
  * @description 소수점이 전부 0인 숫자들 정수로 변환 (메뉴 영양 정보 값)
  * @example 84.000 -> 84
- * @param value
- * @returns
  */
-export function removeTrailingZeros(value: number): number {
+export const removeTrailingZeros = (value: number): number => {
   const stringValue = typeof value === 'number' ? value.toString() : value;
 
   if (stringValue.includes('.')) {
@@ -15,4 +15,14 @@ export function removeTrailingZeros(value: number): number {
   }
 
   return Number(stringValue);
-}
+};
+
+/**
+ * @description api response로 받은 string 배열을 selectbox options 타입으로 변환
+ * @example ['A학교', 'B학교'] -> [{value: 'A학교', label: 'A학교'} , {value: 'B학교', label: 'B학교'}]
+ */
+export const stringArraytoOptionArray = (items: string[]): Option[] =>
+  items.map((item) => ({
+    value: item,
+    label: item,
+  }));
