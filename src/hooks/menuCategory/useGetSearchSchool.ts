876e0f4e6169da720b16c751a1443520ca7cca85
grapefruit <liam.heo11@gmail.com>
2025-04-05 21:32:06 +0900

    fix: 학교명 검색 use 훅에 options 추가

--- src/hooks/menuCategory/useGetSearchSchool.ts
+++ src/hooks/menuCategory/useGetSearchSchool.ts
@@ -3,9 +3,15 @@ import { menuCategories } from '@/api/menuCategory';
 import { GetSearchSchoolRequest } from '@/type/menuCategory/menuCategoryRequest';
 import { menuCategoryKeys } from '@/hooks/menuCategory/queryKey';
 
-export const useGetSearchSchool = (request: GetSearchSchoolRequest) => {
+export const useGetSearchSchool = (
+  request: GetSearchSchoolRequest,
+  options?: {
+    enabled?: boolean;
+  },
+) => {
   return useQuery({
     queryKey: menuCategoryKeys.searchSchool(request),
     queryFn: () => menuCategories.getSearchSchool(request),
+    ...options,
   });
 };
